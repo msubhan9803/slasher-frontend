946473d403cd4b8874edf1b9f1c09b36cce3bb84
Eric O <eric@ejdigitalmedia.com>
2022-05-26 17:02:56 -0400

    CI testing improvements; Fix error during CI build (postcss-svgo:: Non-whitespace before first tag)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
 
 const port = 4444;
 
@@ -13,7 +13,7 @@ const port = 4444;
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './e2e',
+  testDir: "./e2e",
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -21,7 +21,7 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'line' : 'line',
+  reporter: process.env.CI ? "line" : "line",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -41,15 +41,15 @@ const config: PlaywrightTestConfig = {
     viewport: { width: 1280, height: 720 },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome'],
+        ...devices["Desktop Chrome"],
       },
     },
 
@@ -99,9 +99,17 @@ const config: PlaywrightTestConfig = {
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
 
-  /* Run your local dev server before starting the tests */
+  /*
+    Start the webserver so that e2e tests can run:
+    - In the development environment, we run the local dev server (`npm run start`).
+    - In a CI environment, we expect that `npm run build` has already been run, and then we run
+      `npm run serve-build` (instead of `npm run start`). Reason is, `npm run start` can sometimes
+      be slow to start on a CI VM, so running a build avoids timeouts during e2e test startup.
+  */
   webServer: {
-    command: `cross-env PORT=${port} npm run start`,
+    command: process.env.CI
+      ? `npm run serve-build -- -l ${port}`
+      : `cross-env PORT=${port} npm run start`,
     port,
   },
 };

8123ebe4ead8d09aa160e41a85502354e92fdebe
Avadh Ladani <avadh.ladani353@gmail.com>
2022-05-27 18:13:04 +0530

    -create registration identity screen -add stepper in registration component

--- src/routes/registration/Registration.tsx
+++ src/routes/registration/Registration.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import AuthenticatedSiteWrapper from '../../components/layout/main-site-wrapper/authenticated/AuthenticatedSiteWrapper';
+import React, { useState } from 'react';
+import { Step, Stepper } from 'react-form-stepper';
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+} from 'react-router-dom';
+import UnauthenticatedSiteWrapper from '../../components/layout/main-site-wrapper/unauthenticated/UnauthenticatedSiteWrapper';
 import RegistrationIdentity from './RegistrationIdentity';
 import RegistrationSecurity from './RegistrationSecurity';
 
 function Registration() {
+  const navigate = useNavigate();
+  const [goSteps, setGoSteps] = useState<any>(0);
+
+  const nextStep = () => {
+    setGoSteps(goSteps + 1);
+  };
+
   return (
-    <AuthenticatedSiteWrapper>
+    <UnauthenticatedSiteWrapper>
       <div className="registration">
+        <h1 className="h3">Create account</h1>
+        <div>
+          <Stepper activeStep={goSteps}>
+            <Step onClick={() => { setGoSteps(0); navigate('/registration/identity'); }} label="step 1" />
+            <Step onClick={() => { setGoSteps(1); navigate('/registration/security'); }} label="step 2" />
+            <Step onClick={() => { setGoSteps(2); navigate('/registration/identity'); }} label="step 3" />
+          </Stepper>
+        </div>
+        {goSteps}
+        {typeof goSteps}
         <Routes>
           <Route path="/" element={<Navigate to="identity" replace />} />
-          <Route path="identity" element={<RegistrationIdentity />} />
+          <Route path="identity" element={<RegistrationIdentity onClick={nextStep} step={goSteps} />} />
           <Route path="security" element={<RegistrationSecurity />} />
         </Routes>
       </div>
-    </AuthenticatedSiteWrapper>
+    </UnauthenticatedSiteWrapper>
   );
 }
 
--- src/routes/registration/RegistrationIdentity.tsx
+++ src/routes/registration/RegistrationIdentity.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import {
+  Button,
+  Container,
+  Form,
+  Row,
+} from 'react-bootstrap';
 
-function RegistrationIdentity() {
+function RegistrationIdentity({ onClick }: any) {
+  const navigate = useNavigate();
+
+  const handleStep = (e: any) => {
+    navigate('/registration/security');
+    e.preventDefault();
+    onClick();
+  };
+
   return (
-    <div className="registration-identity">
-      <h1>Registration Identity</h1>
-      <p>This is the registration identity page!</p>
-      <Link to="/registration/security">Go to the registration security page</Link>
-    </div>
+    <Container className="mt-4 align-self-center text-center">
+
+      <Form>
+        <Row className="justify-content-center">
+          <Form.Group className="col-md-4 mb-3 text-start">
+            <Form.Control type="text" placeholder="First Name" />
+            <p className="pt-2">
+              This is how your first name will appear in your profile.
+            </p>
+          </Form.Group>
+
+          <Form.Group className="col-md-4 mb-3 text-start">
+            <Form.Control type="text" placeholder="Username" />
+            <p className="pt-2">
+              You can use letters (case sensitive), numbers, and the
+              following special characters: “.”, “-”, or “_”.
+            </p>
+            <p>
+              Special characters cannot be the first or last character of your username.
+            </p>
+            <p>
+              This is how your username will appear when you post and comment on Slasher.
+            </p>
+          </Form.Group>
+
+          <Form.Group className="col-md-4 mb-3 text-start">
+            <Form.Control type="email" placeholder="Email address" />
+            <p className="pt-2">
+              We will send an email with an account activation link to this email address.
+              Be sure to click the link in the email to activate your Slasher account. If
+              you do not activate your account, you will not be able to login.
+            </p>
+          </Form.Group>
+
+          <div className="col-md-4 mt-5">
+            <Button onClick={handleStep} className="w-100" variant="primary" type="submit">
+              Next step
+            </Button>
+          </div>
+        </Row>
+      </Form>
+
+    </Container>
   );
 }
 

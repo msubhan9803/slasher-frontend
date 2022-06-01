8db02309e66359783542cfe9b12816d10b0a0d6f
Ronak Diyora <ronakdiyora@outlook.com>
2022-06-01 17:44:37 +0530

    Set image for large screen in dating add photos

--- src/routes/dating/setup/DatingSetupAddPhotos.tsx
+++ src/routes/dating/setup/DatingSetupAddPhotos.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  Button, Col, Form, Row,
+  Col, Form, Row,
 } from 'react-bootstrap';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import UnauthenticatedSiteWrapper from '../../../components/layout/main-site-wrapper/unauthenticated/UnauthenticatedSiteWrapper';
+import RoundButton from '../../../components/ui/RoundButton';
 
-const Images = [
-  { name: 'img1', val: 'img1' },
-  { name: 'img1', val: 'img1' },
-  { name: 'img1', val: 'img1' },
-  { name: 'img1', val: 'img1' },
-  { name: 'img1', val: 'img1' },
-  { name: 'img1', val: 'img1' },
-];
+interface Image {
+  title: string;
+  image: string;
+  id: number
+}
 function DatingSetupAddPhotos() {
+  const [imageUpload, setImageUpload] = useState<Image[]>([
+    { title: '', image: '', id: 1 },
+    { title: '', image: '', id: 2 },
+    { title: '', image: '', id: 3 },
+    { title: '', image: '', id: 4 },
+    { title: '', image: '', id: 5 },
+    { title: '', image: '', id: 6 },
+  ]);
+  const handleFileChange = (e: any, index: any) => {
+    if (!e.target) {
+      return;
+    }
+    if (e.target?.name === 'file') {
+      const newArr = [...imageUpload];
+      const image = URL.createObjectURL(e.target.files[0]);
+      newArr[index].image = image;
+      setImageUpload(newArr);
+    }
+  };
   return (
     <UnauthenticatedSiteWrapper>
       <Row className="justify-content-center text-center">
         <Col lg={8}>
           <h3>Add Photos</h3>
-          <p>
+          <p className="fw-normal">
             You must add at least one photo to your dating profile, otherwise
             your profile will not be shown to others and you will not be able to see
             other profiles.
@@ -31,24 +48,47 @@ function DatingSetupAddPhotos() {
       <Row xs={12} lg="auto" className="justify-content-center">
         <Col className="d-flex justify-content-center" xs={12} md={4}>
           <FontAwesomeIcon icon={solid('check')} size="lg" className="text-primary" />
-          <p className="mx-2">Use recent photos</p>
+          <p className="mx-2 fw-normal">Use recent photos</p>
         </Col>
         <Col className="d-flex justify-content-center" xs={12} md={4}>
           <FontAwesomeIcon icon={solid('check')} size="lg" className="text-primary" />
-          <p className="mx-2">Be sure your photos are clear</p>
+          <p className="mx-2 fw-normal">Be sure your photos are clear</p>
         </Col>
         <Col className="d-flex justify-content-center" xs={12} md={4}>
           <FontAwesomeIcon icon={solid('check')} size="lg" className="text-primary" />
-          <p className="mx-2">Use photos of you</p>
+          <p className="mx-2 fw-normal">Use photos of you</p>
         </Col>
       </Row>
-      <Row className="justify-content-center text-center h-100">
+      <Row className="justify-content-center text-center h-auto">
         <Col lg={8} className="h-100">
           <Row className="h-100">
-            {Images.map((images) => (
-              <Col key={images.name} xs={4} className="my-3">
-                <div className="rounded-3 d-flex justify-content-center align-items-center  w-auto" style={{ height: '197px', backgroundColor: '#1F1F1F', border: '2px solid #3A3B46' }}>
-                  <FontAwesomeIcon icon={solid('camera')} size="lg" className="text-light bg-primary p-3 rounded-circle " />
+            {imageUpload.map((image, imageIndex) => (
+              <Col key={image.id} xs={4} className="my-3">
+
+                <div className="d-flex justify-content-center align-items-center w-auto " style={{ height: '196px', backgroundColor: '#1F1F1F', border: '2px solid #3A3B46' }}>
+                  <label htmlFor={`file-upload-${imageIndex}`}>
+                    {image.image === ''
+                      ? <FontAwesomeIcon icon={solid('camera')} size="lg" className="text-light bg-primary p-3 rounded-circle " />
+                      : (
+                        <img
+                          src={image.image}
+                          alt="UploadImage.."
+                          style={{ height: '196px', width: '226px' }}
+                          onChange={(e: any) => handleFileChange(e, imageIndex)}
+                        />
+                      )}
+                    <input
+                      key={image.id}
+                      id={`file-upload-${imageIndex}`}
+                      type="file"
+                      name="file"
+                      className="d-none"
+                      accept="image/*"
+                      onChange={(e) => {
+                        handleFileChange(e, imageIndex);
+                      }}
+                    />
+                  </label>
                 </div>
                 <Form.Check
                   inline
@@ -65,14 +105,14 @@ function DatingSetupAddPhotos() {
         </Col>
         <Row className="justify-content-center">
           <Col md={5} className="mt-3">
-            <Button
+            <RoundButton
               variant="primary"
               type="submit"
               className="w-100 px-5"
               size="lg"
             >
               Next Step
-            </Button>
+            </RoundButton>
           </Col>
         </Row>
       </Row>

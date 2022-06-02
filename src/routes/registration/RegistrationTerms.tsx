956804056433247f0c8a091915623ce9caf48dc3
Eric O <eric@ejdigitalmedia.com>
2022-06-01 23:10:13 -0400

    Store terms and policies content in re-usable components

--- src/routes/registration/RegistrationTerms.tsx
+++ src/routes/registration/RegistrationTerms.tsx
@@ -5,6 +5,10 @@ import {
   Row,
 } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import CommunityStandardsAndRules from '../../components/terms-and-policies/CommunityStandardsAndRules';
+import EndUserLicenseAgreement from '../../components/terms-and-policies/EndUserLicenseAgreement';
+import PrivacyPolicy from '../../components/terms-and-policies/PrivacyPolicy';
+import TermsAndConditions from '../../components/terms-and-policies/TermsAndConditions';
 import RoundButton from '../../components/ui/RoundButton';
 
 function RegistrationTerms({ changeStep }: any) {
@@ -14,31 +18,16 @@ function RegistrationTerms({ changeStep }: any) {
     changeStep(3);
   };
   return (
-    <Container className="">
+    <Container>
       <h1 className="h3">
         Please scroll down to review our Terms and Conditions,
         Privacy Policy, End User License Agreement, and Community Standards
       </h1>
-      <h2 className="h3 mt-5">Terms and Conditions</h2>
-      <p>
-        Contrary to popular belief, Lorem Ipsum is not simply random text.
-        It has roots in a piece of classical Latin literature from 45 BC,
-        making it over 2000 years old. Richard McClintock, a Latin professor
-        at Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through the
-        cites of the word in classical literature, discovered the undoubtable source.
-        Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-      </p>
-      <h1 className="h3 mt-5">Privacy</h1>
-      <p>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-        in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked
-        up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-        going through the cites of the word in classical literature, discovered the undoubtable
-        source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-      </p>
-      <h1 className="h3 mt-5 pt-4">I agree</h1>
+      <TermsAndConditions className="mt-5" />
+      <PrivacyPolicy className="mt-5" />
+      <EndUserLicenseAgreement className="mt-5" />
+      <CommunityStandardsAndRules className="mt-5" />
+      <h1 className="h3 mt-5">I agree</h1>
       <p>
         By clicking Sign up, you agree that you are at least 17 years of age, and that you agree
         with our Terms and Conditions, Privacy Policy, End User License Agreement, and Community

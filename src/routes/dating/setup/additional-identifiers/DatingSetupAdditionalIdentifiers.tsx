5fd186ad4750da64800602efb3128076d29349b1
Ronak Diyora <ronakdiyora@outlook.com>
2022-06-07 16:21:00 +0530

    add one common component for option
    - change button into checkbox in large screen

--- src/routes/dating/setup/additional-identifiers/DatingSetupAdditionalIdentifiers.tsx
+++ src/routes/dating/setup/additional-identifiers/DatingSetupAdditionalIdentifiers.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable max-lines */
 import React, { useState } from 'react';
 import {
-  Button, Col, Form, Row,
+  Col, Form, Row,
 } from 'react-bootstrap';
 import UnauthenticatedSiteWrapper from '../../../../components/layout/main-site-wrapper/unauthenticated/UnauthenticatedSiteWrapper';
 import RoundButton from '../../../../components/ui/RoundButton';
@@ -21,9 +20,9 @@ import {
   bodyTypeOptions,
   interestsList,
 } from './additional-identifiers-form-options';
+import DatingIdentifierSelect from './DatingIdentifierSelect';
 
 function DatingSetupAdditionalIdentifiers() {
-  const [interest, setInterest] = useState('');
   const [newToArea, setNewToArea] = useState(newToAreaOptions[0].value);
 
   return (
@@ -31,157 +30,106 @@ function DatingSetupAdditionalIdentifiers() {
       <Form>
         <Row>
           <Col sm={12} md={3}>
-            <p className="m-1">Relationship Status</p>
-            <Form.Select aria-label="Relationship Status selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {relationshipStatusOptions.map((relation) => (
-                <option key={relation.value} value={relation.value}>{relation.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="relationship"
+              options={relationshipStatusOptions}
+              label="Relationship Status"
+            />
           </Col>
           <Col sm={12} md={3}>
-            <p className="m-1">Body Type</p>
-            <Form.Select aria-label="Body Type selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {bodyTypeOptions.map((body) => (
-                <option key={body.value} value={body.value}>{body.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="bodyType"
+              options={bodyTypeOptions}
+              label="Body Type"
+            />
           </Col>
           <Col sm={12} md={3}>
-            <p className="m-1">Tattoos</p>
-            <Form.Select aria-label="Tattoos selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {tatoosOptions.map((tatoo) => (
-                <option key={tatoo.value} value={tatoo.value}>{tatoo.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="tattoos"
+              options={tatoosOptions}
+              label="Tattoos"
+            />
           </Col>
           <Col sm={12} md={3}>
-            <p className="m-1">Ethnicity</p>
-            <Form.Select aria-label="Ethnicity selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {ethnicityOptions.map((ethnicity) => (
-                <option key={ethnicity.value} value={ethnicity.value}>{ethnicity.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="ethnicity"
+              options={ethnicityOptions}
+              label="Ethnicity"
+            />
           </Col>
         </Row>
 
         <Row className="mt-4">
           <Col sm={12} md={6}>
-            <p className="m-1">Parental Status</p>
-            <Form.Select aria-label="Parental Status selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {parentalStatusOptions.map((parental) => (
-                <option key={parental.value} value={parental.value}>{parental.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="parentalStatus"
+              options={parentalStatusOptions}
+              label="Parental Status"
+            />
           </Col>
           <Col sm={12} md={6}>
-            <p className="m-1">Religion</p>
-            <Form.Select aria-label="Religion selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {religionOptions.map((religion) => (
-                <option key={religion.value} value={religion.value}>{religion.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="Religion"
+              options={religionOptions}
+              label="Religion"
+            />
           </Col>
         </Row>
 
         <Row className="mt-4">
           <Col sm={12} md={6}>
-            <p className="m-1">Education Level</p>
-            <Form.Select aria-label="Education Level selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {educationLevelOptions.map((education) => (
-                <option key={education.value} value={education.value}>{education.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="educationLevel"
+              options={educationLevelOptions}
+              label="Education Level"
+            />
           </Col>
           <Col sm={12} md={6}>
-            <p className="m-1">Employment</p>
-            <Form.Select aria-label="Employment selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {employmentOptions.map((employment) => (
-                <option key={employment.value} value={employment.value}>{employment.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="employment"
+              options={employmentOptions}
+              label="Employment"
+            />
           </Col>
         </Row>
 
         <Row className="mt-4">
           <Col sm={12} md={4}>
-            <p className="m-1">Pets</p>
-            <Form.Select aria-label="Pets selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {petOptions.map((pet) => (
-                <option key={pet.value} value={pet.value}>{pet.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="pets"
+              options={petOptions}
+              label="Pets"
+            />
           </Col>
           <Col sm={12} md={4}>
-            <p className="m-1">Drinking</p>
-            <Form.Select aria-label="Drinking selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {drinkingOptions.map((drink) => (
-                <option key={drink.value} value={drink.value}>{drink.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="drinking"
+              options={drinkingOptions}
+              label="Drinking"
+            />
           </Col>
           <Col sm={12} md={4}>
-            <p className="m-1">Smoking</p>
-            <Form.Select aria-label="Smoking selection" defaultValue="">
-              <option value="" disabled>
-                Select one
-              </option>
-              {smokingOptions.map((smok) => (
-                <option key={smok.value} value={smok.value}>{smok.label}</option>
-              ))}
-            </Form.Select>
+            <DatingIdentifierSelect
+              name="smoking"
+              options={smokingOptions}
+              label="Smoking"
+            />
           </Col>
         </Row>
         <h4 className="mt-5">Interests</h4>
-        <Row className="mt-3 ">
+        <Row className="mt-3">
           {interestsList.map((interests: string) => (
             <Col xs={4} md={2} key={interests} className="d-md-block d-none">
-              <Button
-                variant="form"
-                size="lg"
-                className="w-100 my-1 fs-6"
-                name={interests}
-                active={interests === interest}
-                onClick={(e: any) => setInterest(e.target.name)}
-              >
+              <input type="checkbox" className="btn-check" id={interests} />
+              <label className="btn btn-form btn-lg fs-6 my-1 w-100" htmlFor={interests}>
                 {interests}
-              </Button>
+              </label>
             </Col>
           ))}
         </Row>
-        {/* mobile view for interest list */}
         <Row>
           {interestsList.map((check: any, index: any) => (
-            <Col xs={6} className="d-block d-sm-none" key={check}>
+            <Col xs={6} className="d-block d-md-none" key={check}>
               <Form.Check
                 type="checkbox"
                 id={`${check} - ${index}`}
@@ -206,7 +154,10 @@ function DatingSetupAdditionalIdentifiers() {
 
         <Row className="mt-4 justify-content-center">
           <Col md={4}>
-            <RoundButton className="w-100" type="submit">
+            <RoundButton
+              className="w-100"
+              type="submit"
+            >
               Next step
             </RoundButton>
           </Col>

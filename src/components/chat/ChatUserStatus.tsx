8d9490ca693ee4ef1252160b102de5eec72031e1
Ronak Diyora <ronakdiyora@outlook.com>
2022-06-17 10:01:40 +0530

    fix padding for chat status

--- src/components/chat/ChatUserStatus.tsx
+++ src/components/chat/ChatUserStatus.tsx
@@ -20,7 +20,7 @@ function ChatUserStatus() {
       <Col xs={2} lg={1} className="ps-3">
         <ChatProfileStyleImage src="https://i.pravatar.cc/150?img=19" alt="Chat Other Profile" />
       </Col>
-      <Col xs={9} sm={10} lg={11} className="ps-2">
+      <Col xs={9} sm={10} lg={11} className="ps-4 ps-sm-2 ps-lg-3">
         <h6 className="mb-0">Eliza Williams</h6>
         <p className="mb-0 small text-success">Online</p>
       </Col>

6924f0df8f2cb3b652c2db8400ff498b8afd6576
Avadh Ladani <avadh.ladani353@gmail.com>
2022-07-22 18:17:49 +0530

    fix all design issue in create post

--- src/routes/posts/create-post/CreatePost.tsx
+++ src/routes/posts/create-post/CreatePost.tsx
@@ -2,7 +2,7 @@ import React, {
   ChangeEvent, useRef, useState,
 } from 'react';
 import {
-  Button, Col, Form, Image, Row,
+  Col, Form, Image, Row,
 } from 'react-bootstrap';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,14 +11,13 @@ import AuthenticatedPageWrapper from '../../../components/layout/main-site-wrapp
 import RoundButton from '../../../components/ui/RoundButton';
 
 const UserCircleImage = styled(Image)`
-  height: 3.125rem;
-  width: 3.125rem;
+  height:5rem;
+  width: 5rem;
 `;
 
 const ImageContainer = styled.div`
-  width: 6.25rem;
-  height: 6.25rem;
-  background-color: #1F1F1F;
+  width:7.25rem;
+  height:7.25rem;
   border: 0.125rem solid #3A3B46
 `;
 
@@ -51,73 +50,46 @@ function CreatePost() {
 
   return (
     <AuthenticatedPageWrapper rightSidebarType="profile-self">
-      <h1 className="h3">Create Post</h1>
-      <Form className="bg-dark px-3 py-4 rounded-2">
-        <Form.Group className="mb-4" controlId="about-me">
-          <Form.Label className="align-items-center d-flex form-label mb-4 px-2 w-100 mb-4">
+      <Row className="d-md-none bg-dark">
+        <Col xs="auto" className="ms-2"><FontAwesomeIcon role="button" icon={solid('arrow-left-long')} size="2x" /></Col>
+        <Col><h1 className="h4 text-center">Create Post</h1></Col>
+      </Row>
+      <p className="fs-4 fw-bold text-center text-md-start d-none d-md-block">Create Post</p>
+      <Form className="bg-dark px-4 py-4 rounded-2">
+        <Form.Group className=" mx-2 " controlId="about-me">
+          <div className="align-items-center d-flex form-label mb-4  w-100 mb-4">
             <UserCircleImage src="https://i.pravatar.cc/300?img=12" className="rounded-circle me-3" />
-            <h2 className="rounded-circle h5 mb-0">
+            <p className="fs-4 fw-bold mb-0 align-self-center">
               Aly Khan
-            </h2>
-          </Form.Label>
+            </p>
+          </div>
           <Form.Control
-            rows={8}
+            rows={12}
             as="textarea"
             value={postContent}
             onChange={(contentEvent) => setPostContent(contentEvent.target.value)}
-            placeholder="Write something....."
+            placeholder="Create a post"
+            style={{ resize: 'none' }}
+            className="border-0 mb-0 pb-0"
           />
         </Form.Group>
-        {uploadPost.length > 0 && (
-          <Row className="mb-4 d-none d-md-flex">
-            {uploadPost.map((post: string) => (
-              <Col sm={4} lg={2} key={post} className="mb-3">
-                <ImageContainer className="position-relative d-flex justify-content-center align-items-center rounded border-0">
-                  <Image
-                    src={post}
-                    alt="Dating profile photograph"
-                    className="w-100 h-100 img-fluid rounded"
-                  />
-                  <FontAwesomeIcon
-                    icon={solid('times')}
-                    size="xs"
-                    role="button"
-                    className="position-absolute bg-white text-primary rounded-circle"
-                    style={{
-                      padding: '0.313rem 0.438rem',
-                      top: '5.313rem',
-                      left: '5.313rem',
-                    }}
-                    onClick={() => handleRemoveFile(post)}
-                  />
-                </ImageContainer>
-              </Col>
-            ))}
-          </Row>
-        )}
-        <Row className="justify-content-between">
-          <Col md={4} lg={3} className="mb-3 mb-md-0">
-            <input
-              type="file"
-              name="post"
-              className="d-none"
-              accept="image/*"
-              onChange={(post) => {
-                handleFileChange(post);
-              }}
-              multiple
-              ref={inputFile}
-            />
-            <Button disabled={uploadPost.length >= 10} className="btn btn-form bg-dark w-100 rounded-5" onClick={() => inputFile.current?.click()}>
-              <FontAwesomeIcon icon={regular('image')} className="me-2" />
-              Add photos
-            </Button>
-          </Col>
-          <Col xs={12} className="d-md-none">
+        <input
+          type="file"
+          name="post"
+          className="d-none"
+          accept="image/*"
+          onChange={(post) => {
+            handleFileChange(post);
+          }}
+          multiple
+          ref={inputFile}
+        />
+        <Row>
+          <Col xs={12} className="order-1 order-md-0">
             <Row>
               {uploadPost.map((post: string) => (
-                <Col xs={4} key={post} className="mb-3">
-                  <ImageContainer className="position-relative d-flex justify-content-center align-items-center rounded border-0">
+                <Col xs="auto" key={post} className="mb-1">
+                  <ImageContainer className=" ms-2 mt-4 position-relative d-flex justify-content-center align-items-center rounded border-0">
                     <Image
                       src={post}
                       alt="Dating profile photograph"
@@ -130,8 +102,8 @@ function CreatePost() {
                       className="position-absolute bg-white text-primary rounded-circle"
                       style={{
                         padding: '0.313rem 0.438rem',
-                        top: '5.313rem',
-                        left: '5.313rem',
+                        top: '6.313rem',
+                        left: '6.313rem',
                       }}
                       onClick={() => handleRemoveFile(post)}
                     />
@@ -140,8 +112,14 @@ function CreatePost() {
               ))}
             </Row>
           </Col>
-          <Col md={3} lg={2}>
-            <RoundButton className="w-100" type="submit">
+          <Col md={3} className="mb-3 mb-md-0 order-0 order-md-1 me-auto">
+            <RoundButton disabled={uploadPost.length >= 10} style={{ backgroundColor: '#1F1F1F' }} className="mt-4 border-0 mx-2 btn btn-form w-100 rounded-5 py-2" onClick={() => inputFile.current?.click()}>
+              <FontAwesomeIcon icon={regular('image')} className="me-2" />
+              Add photos
+            </RoundButton>
+          </Col>
+          <Col md={3} className="order-2 ms-auto me-2">
+            <RoundButton className="me-2 mt-4 w-100" type="submit">
               Post
             </RoundButton>
           </Col>
@@ -150,5 +128,4 @@ function CreatePost() {
     </AuthenticatedPageWrapper>
   );
 }
-
 export default CreatePost;

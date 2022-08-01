8272d28964b9ab1aa94041ed51fdcf40653f4983
Avadh Ladani <avadh.ladani353@gmail.com>
2022-08-01 19:40:47 +0530

    remove extra margin for alignment

--- src/routes/posts/create-post/CreatePost.tsx
+++ src/routes/posts/create-post/CreatePost.tsx
@@ -11,14 +11,17 @@ import AuthenticatedPageWrapper from '../../../components/layout/main-site-wrapp
 import RoundButton from '../../../components/ui/RoundButton';
 
 const UserCircleImage = styled(Image)`
-  height:5rem;
-  width: 5rem;
+  height:3.125rem;
+  width:3.125rem;
 `;
 
 const ImageContainer = styled.div`
   width:7.25rem;
   height:7.25rem;
   border: 0.125rem solid #3A3B46
+`;
+const AddPhotosButton = styled(RoundButton)`
+  background-color: #1F1F1F !important;
 `;
 
 function CreatePost() {
@@ -54,14 +57,13 @@ function CreatePost() {
         <Col xs="auto" className="ms-2"><FontAwesomeIcon role="button" icon={solid('arrow-left-long')} size="2x" /></Col>
         <Col><h1 className="h4 text-center">Create Post</h1></Col>
       </Row>
-      <p className="fs-4 fw-bold text-center text-md-start d-none d-md-block">Create Post</p>
       <Form className="bg-dark px-4 py-4 rounded-2">
-        <Form.Group className=" mx-2 " controlId="about-me">
+        <Form.Group controlId="about-me">
           <div className="align-items-center d-flex form-label mb-4  w-100 mb-4">
             <UserCircleImage src="https://i.pravatar.cc/300?img=12" className="rounded-circle me-3" />
-            <p className="fs-4 fw-bold mb-0 align-self-center">
+            <h2 className="h3 mb-0 align-self-center">
               Aly Khan
-            </p>
+            </h2>
           </div>
           <Form.Control
             rows={12}
@@ -69,8 +71,8 @@ function CreatePost() {
             value={postContent}
             onChange={(contentEvent) => setPostContent(contentEvent.target.value)}
             placeholder="Create a post"
-            style={{ resize: 'none' }}
-            className="border-0 mb-0 pb-0"
+            style={{ resize: 'none', backgroundColor: '#0F0F0F' }}
+            className="border-0 mb-0 pb-0 fs-5"
           />
         </Form.Group>
         <input
@@ -89,7 +91,7 @@ function CreatePost() {
             <Row>
               {uploadPost.map((post: string) => (
                 <Col xs="auto" key={post} className="mb-1">
-                  <ImageContainer className=" ms-2 mt-4 position-relative d-flex justify-content-center align-items-center rounded border-0">
+                  <ImageContainer className="mt-4 position-relative d-flex justify-content-center align-items-center rounded border-0">
                     <Image
                       src={post}
                       alt="Dating profile photograph"
@@ -112,15 +114,15 @@ function CreatePost() {
               ))}
             </Row>
           </Col>
-          <Col md={3} className="mb-3 mb-md-0 order-0 order-md-1 me-auto">
-            <RoundButton disabled={uploadPost.length >= 10} style={{ backgroundColor: '#1F1F1F' }} className="mt-4 border-0 mx-2 btn btn-form w-100 rounded-5 py-2" onClick={() => inputFile.current?.click()}>
+          <Col md="auto" className="mb-3 mb-md-0 order-0 order-md-1 me-auto">
+            <AddPhotosButton size="md" disabled={uploadPost.length >= 10} className="mt-4 border-0 btn btn-form w-100 rounded-5 py-2" onClick={() => inputFile.current?.click()}>
               <FontAwesomeIcon icon={regular('image')} className="me-2" />
-              Add photos
-            </RoundButton>
+              <span className="h3">Add photos</span>
+            </AddPhotosButton>
           </Col>
-          <Col md={3} className="order-2 ms-auto me-2">
-            <RoundButton className="me-2 mt-4 w-100" type="submit">
-              Post
+          <Col md="auto" className="order-2 ms-auto">
+            <RoundButton className="px-4 mt-4 w-100" size="md">
+              <span className="h3">Post</span>
             </RoundButton>
           </Col>
         </Row>

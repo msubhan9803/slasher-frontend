5fee00db793240cbffc32e46b2f4a0818f1ef7b9
Avadh Ladani <avadh.ladani353@gmail.com>
2022-08-01 11:12:28 +0530

    fix spacing issue in add your movie screen

--- src/routes/movies/AddYourMovie.tsx
+++ src/routes/movies/AddYourMovie.tsx
@@ -73,15 +73,15 @@ function AddYourMovie() {
       </Row>
       <div className="bg-dark px-md-4 py-4 py-md-5 rounded-3 bg-mobile-transparent">
         <div className="d-flex justify-content-between">
-          <h2>Add your movie and reach horror fans looking for movies on Slasher!</h2>
+          <h2 className="mb-0">Add your movie and reach horror fans looking for movies on Slasher!</h2>
           <div className="d-none d-md-block">
             <span className="text-primary h2">$20.00</span>
             <small className="text-light">/month</small>
           </div>
         </div>
-        <div className="mt-1">
+        <div className="my-3">
           <Text className="mb-0 fs-4">Save time and build your audience by listing your movies.</Text>
-          <Text className="fs-4 mb-2">Here’s what you get:</Text>
+          <Text className="mb-0 fs-4">Here’s what you get:</Text>
         </div>
         <div>
           {noteList.map((notes: string) => (
@@ -144,45 +144,42 @@ function AddYourMovie() {
             />
             <h3 className="text-center mb-1 mt-3">Upload cover art</h3>
             <CustomText className="text-center fs-5 mb-0">Recommended size:</CustomText>
-            <CustomText className="text-center fs-5 ">600x900 pixels (jpg, png)</CustomText>
+            <CustomText className="text-center fs-5 ">600x900 pixels (Jpg, Png)</CustomText>
           </Col>
           <Col>
-            <Form>
-              <Row>
-                <Col>
-                  <Form.Control type="text" placeholder="Title" className="fs-4" />
-                </Col>
-              </Row>
-              <Row className="mt-3">
-                <Col>
-                  <Form.Group className="mb-3 fs-5" controlId="Overview">
-                    <Form.Control
-                      maxLength={1000}
-                      rows={6}
-                      as="textarea"
-                      value={description}
-                      onChange={handleMessageChange}
-                      placeholder="Overview"
-                      style={{ resize: 'none' }}
-                      className="fs-4"
-                    />
-                    <CustomSpan className="float-end fs-5">{`${charCount}/${1000} characters`}</CustomSpan>
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Form.Control type="text" placeholder="Movie trailer (YouTube link)" className="fs-5" />
-                </Col>
-              </Row>
-              <p className="my-4 fs-4">You can add more details later on the “Edit movie” screen.</p>
-              <Row className="">
-                <Col md={4} className="mt-1">
-                  <RoundButton className="w-100 p-1 fs-3" variant="primary" size="lg" type="submit">Submit</RoundButton>
-                </Col>
-              </Row>
-
-            </Form>
+            <Row>
+              <Col>
+                <Form.Control type="text" placeholder="Title" className="fs-4" />
+              </Col>
+            </Row>
+            <Row className="mt-3">
+              <Col>
+                <Form.Group className="mb-3 fs-5" controlId="Overview">
+                  <Form.Control
+                    maxLength={113}
+                    rows={6}
+                    as="textarea"
+                    value={description}
+                    onChange={handleMessageChange}
+                    placeholder="Overview"
+                    style={{ resize: 'none' }}
+                    className="fs-4"
+                  />
+                  <CustomSpan className="float-end fs-5">{`${charCount}/${113} characters`}</CustomSpan>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Control type="text" placeholder="Movie trailer (YouTube link)" className="fs-5" />
+              </Col>
+            </Row>
+            <p className="my-4 fs-4">You can add more details later on the “Edit movie” screen.</p>
+            <Row>
+              <Col md={4} className="mt-1">
+                <RoundButton className="w-100 p-1 fs-3" size="lg">Submit</RoundButton>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </div>

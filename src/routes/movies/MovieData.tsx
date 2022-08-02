40bec96244f4e874c869df20f012429367e7b779
Ronak Diyora <ronakdiyora@outlook.com>
2022-08-02 10:36:45 +0530

    - set route to add new movies - create new file for movies data

---
+++ src/routes/movies/MovieData.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import {
+  Col, Container, Row, Tab, Tabs,
+} from 'react-bootstrap';
+import styled from 'styled-components';
+import AuthenticatedPageWrapper from '../../components/layout/main-site-wrapper/authenticated/AuthenticatedPageWrapper';
+import CustomScrollbar from '../../components/ui/CustomScrollbar';
+import RoundButton from '../../components/ui/RoundButton';
+import MoviesFilterComponent from './components/MoviesFilterComponent';
+import MoviesFilterOptions from './components/MoviesFilterOptions';
+import MoviesSearch from './components/MoviesSearch';
+import MoviesSort from './components/MoviesSort';
+import MovieCard from '../../components/movie/MovieCard';
+
+const StyleTabs = styled(CustomScrollbar)`
+  overflow-x: scroll;
+  overflow-y: hidden;
+  .nav-link {
+    padding-bottom: 1rem;
+    border: none;
+    color: #ffffff;
+    &:hover {
+      border-color: transparent;
+      color: var(--bs-primary);
+    }
+    &.active {
+      color: var(--bs-primary);
+      background-color: transparent;
+      border-bottom:  0.188rem solid var(--bs-primary);
+    }
+  }
+`;
+function MovieData() {
+  const tabs = [
+    { value: 'allMovies', label: 'All movies' },
+    { value: 'myMovies', label: 'My movies' },
+    { value: 'slasherIndie', label: 'Slasher Indie' },
+    { value: 'favoritesList', label: 'Favorites list' },
+    { value: 'watchList', label: 'Watch list' },
+    { value: 'watchedList', label: 'Watched list' },
+    { value: 'buyList', label: 'Buy list' },
+  ];
+  const myMovies = [
+    {
+      id: 1, name: 'Dreamcatcher: Get ready for a killer night out', year: '2022', liked: true,
+    },
+    {
+      id: 2, name: 'The Djinn', year: '2022', liked: false,
+    },
+    {
+      id: 3, name: 'Ghost Lab', year: '2022', liked: true,
+    },
+    {
+      id: 4, name: 'Dreamcatcher: Get ready for a killer night out', year: '2022', liked: true,
+    },
+    {
+      id: 5, name: 'The Exorcist', year: '2022', liked: true,
+    },
+    {
+      id: 6, name: 'Spiral: From the Book of Saw', year: '2022', liked: false,
+    },
+    {
+      id: 7, name: 'The Night House', year: '2022', liked: true,
+    },
+    {
+      id: 8, name: 'Zombie Reddy', year: '2022', liked: true,
+    },
+  ];
+  const [showKeys, setShowKeys] = useState(false);
+  const [filteredMovies, setFilteredMovies] = useState(myMovies);
+  return (
+    <AuthenticatedPageWrapper rightSidebarType="movie">
+      <Container fluid>
+        <h1 className="d-lg-none h4 text-center">Movies</h1>
+        <Row className="bg-dark bg-mobile-transparent rounded-3">
+          <Col xs={12}>
+            <StyleTabs>
+              <Tabs className="border-0 justify-content-between flex-nowrap mt-3">
+                {tabs.map(({ value, label }) => (
+                  <Tab key={value} eventKey={value} title={label} />
+                ))}
+              </Tabs>
+            </StyleTabs>
+          </Col>
+        </Row>
+        <Row className="my-4 align-items-center">
+          <Col md={4} className="my-3 my-md-0 order-md-second order-md-first">
+            <MoviesSearch setFilteredMovies={setFilteredMovies} myMovies={myMovies} />
+          </Col>
+          <Col md={4} className="text-center">
+            <MoviesFilterOptions setShowKeys={setShowKeys} showKeys={showKeys} />
+          </Col>
+          <Col md={4} className="d-none d-lg-block">
+            <MoviesSort title="Sort: " className="rounded-5" />
+          </Col>
+          <Col md={4} className="order-first order-md-last">
+            <RoundButton className="py-2 d-lg-none w-100">Add your movie</RoundButton>
+          </Col>
+        </Row>
+        {showKeys && (<MoviesFilterComponent showKeys={showKeys} setShowKeys={setShowKeys} />)}
+        <div className="bg-dark bg-mobile-transparent rounded-3 py-1 px-lg-2">
+          <Row className="mt-3 mx-lg-0">
+            {filteredMovies.length > 0 ? filteredMovies.map((movieDetail) => (
+              <Col xs={4} md={3} lg={4} xl={3} key={movieDetail.id}>
+                <MovieCard
+                  name={movieDetail.name}
+                  year={movieDetail.year}
+                  liked={movieDetail.liked}
+                />
+              </Col>
+            )) : (
+              <h1 className="h4 text-center mb-0">No data found</h1>
+            )}
+          </Row>
+        </div>
+      </Container>
+    </AuthenticatedPageWrapper>
+  );
+}
+
+export default MovieData;
--- src/routes/movies/components/MovieSidebar.tsx
+++ src/routes/movies/components/MovieSidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AdvertisementBox from '../../../components/layout/right-sidebar-wrapper/components/AdvertisementBox';
 import FriendRequests from '../../../components/layout/right-sidebar-wrapper/components/FriendRequests';
 import RecentMessages from '../../../components/layout/right-sidebar-wrapper/components/RecentMessages';
@@ -7,7 +8,9 @@ import RoundButton from '../../../components/ui/RoundButton';
 function MovieSidebar() {
   return (
     <>
-      <RoundButton className="w-100 mb-4 fs-4">Add your movie</RoundButton>
+      <Link to="/movies/add">
+        <RoundButton className="w-100 mb-4 fs-4">Add your movie</RoundButton>
+      </Link>
       <AdvertisementBox />
       <RecentMessages />
       <FriendRequests />

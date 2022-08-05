7d4cbc0e885092930aa330b034d27dda83749d68
Ronak Diyora <ronakdiyora@outlook.com>
2022-08-05 22:55:07 +0530

    remove back arrow for mobile view

--- src/components/chat/ChatUserStatus.tsx
+++ src/components/chat/ChatUserStatus.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Image } from 'react-bootstrap';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 const ChatProfileStyleImage = styled(Image)`
   height: 3.125rem;
@@ -13,9 +10,6 @@ const ChatProfileStyleImage = styled(Image)`
 function ChatUserStatus() {
   return (
     <div className="align-items-center d-flex">
-      <Link to="/messages/conversation/1" className="d-lg-none">
-        <FontAwesomeIcon icon={solid('arrow-left')} size="2x" className="text-white border-end-0" />
-      </Link>
       <ChatProfileStyleImage src="https://i.pravatar.cc/300?img=19" className="mx-3 ms-lg-0 rounded-circle bg-secondary" />
       <h1 className="h3 mb-0">Eliza Williams</h1>
     </div>

1b6357bbaaaa5ca52837f5dc3701e06f70348d18
Ronak Diyora <ronakdiyora@outlook.com>
2022-08-17 10:05:55 +0530

    - create dating tutorial SD-306 Dating Messages screen - set navigation link on likes Next button

--- src/routes/dating/tutorial/DatingMessages.tsx
+++ src/routes/dating/tutorial/DatingMessages.tsx
@@ -1,27 +1,27 @@
+import { regular } from '@fortawesome/fontawesome-svg-core/import.macro';
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import DatingPageWrapper from '../components/DatingPageWrapper';
 import TutorialButtons from './components/TutorialButtons';
 import TutorialHeader from './components/TutorialHeader';
 import TutorialIcon from './components/TutorialIcon';
 import TutorialPhotos from './components/TutorialPhotos';
-import PassDeckDesktop from '../../../images/pass-deck-desktop.jpg';
-import PassDeckMobile from '../../../images/pass-deck-mobile.jpg';
+import DatingMessagesDesktop from '../../../images/dating-messages-desktop.jpg';
+import DatingMessagesMobile from '../../../images/dating-messages-mobile.jpg';
 
-function Like() {
+function DatingMessages() {
   return (
     <DatingPageWrapper>
       <div className="text-center p-3 mt-5 pt-5 mt-lg-0 pt-lg-3">
         <TutorialHeader />
-        <TutorialPhotos desktopImage={PassDeckDesktop} mobileImage={PassDeckMobile} />
+        <TutorialPhotos desktopImage={DatingMessagesDesktop} mobileImage={DatingMessagesMobile} />
         <Row className="justify-content-center">
           <Col xs={8}>
-            <TutorialIcon tutorialIcon={solid('heart')} iconColor="#32D74B" uniqueId="like" />
-            <h2 className="h2 fw-bold mt-4">Like</h2>
+            <TutorialIcon tutorialIcon={regular('comments')} iconColor="#FF1800" uniqueId="dating-messages" />
+            <h2 className="h2 fw-bold mt-4">Dating messages</h2>
             <p className="fs-4">
-              On a user’s profile, the heart icon is to like
-              this person and let me know you’re interested.
+              In the Dating Menu, the messages icon will allow
+              you to go to your inbox and view your messages.
             </p>
           </Col>
         </Row>
@@ -31,4 +31,4 @@ function Like() {
   );
 }
 
-export default Like;
+export default DatingMessages;
--- src/routes/dating/tutorial/DatingTutorial.tsx
+++ src/routes/dating/tutorial/DatingTutorial.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import DatingMessages from './DatingMessages';
 import Like from './Like';
 import MonsterLike from './MonsterLike';
 import PassDeck from './PassDeck';
@@ -12,6 +13,7 @@ function DatingTutorial() {
       <Route path="undo" element={<Undo />} />
       <Route path="monster-likes" element={<MonsterLike />} />
       <Route path="like" element={<Like />} />
+      <Route path="dating-messages" element={<DatingMessages />} />
     </Routes>
   );
 }

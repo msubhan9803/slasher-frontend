b4dec782a79cb3da9c675c89f08faf0cdd7a1631
Eric O <eric@ejdigitalmedia.com>
2022-08-25 22:21:07 -0400

    Add no-console eslint rule to catch temporary, debugging console log lines

--- api-v2/src/main.ts
+++ api-v2/src/main.ts
@@ -5,8 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
   const port = config.get<number>('PORT', 4000);
+  // eslint-disable-next-line no-console
   console.log(`Starting on port: ${port}`);
+
   await app.listen(port);
 }
 bootstrap();

11c997845ce8d832cb90e2c4467c511d354f0380
Avadh Ladani <avadh.ladani353@gmail.com>
2022-09-01 16:57:59 +0530

    - design modal on click of location icon - create style component for Mapcontainer

--- src/routes/events/EventsByLocation/EventsByLocation.tsx
+++ src/routes/events/EventsByLocation/EventsByLocation.tsx
@@ -9,11 +9,27 @@ import ReactDOMServer from 'react-dom/server';
 import Leaflet from 'leaflet';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import styled from 'styled-components';
 import AuthenticatedPageWrapper from '../../../components/layout/main-site-wrapper/authenticated/AuthenticatedPageWrapper';
 import EventHeader from '../EventHeader';
 import EventsPosterCard from '../EventsPosterCard';
 import EventPoster from '../../../images/events-poster.png';
 import 'leaflet/dist/leaflet.css';
+
+const Map = styled(MapContainer)`
+  height: 400px !important;
+  width: 100% !important;
+  .leaflet-div-icon {
+    background: transparent !important;
+    border: none !important;
+  }
+  .leaflet-popup-content{
+    margin: 13px 20px 13px 16px;
+  }
+  .leaflet-popup {
+    bottom : 0 !important;
+  }
+`;
 
 const iconHTML = ReactDOMServer.renderToString(<FontAwesomeIcon icon={solid('location-dot')} size="3x" className="text-primary" />);
 const customMarkerIcon = new Leaflet.DivIcon({
@@ -32,7 +48,7 @@ const eventsList = [
 function EventsByLocation() {
   const mapRef = useRef() as any;
   const ZOOM_LEVEL = 9;
-  const [center, setCenter] = useState<any>([0, 0]);
+  const [center, setCenter] = useState<any>([42.519539, -70.896713]);
 
   const onSuccess = useCallback((position: any) => {
     const userLocation = [position.coords.latitude, position.coords.longitude];
@@ -47,7 +63,7 @@ function EventsByLocation() {
     <AuthenticatedPageWrapper rightSidebarType="event">
       <EventHeader tabKey="by-location" />
       <div className="mt-3 bg-dark bg-mobile-transparent p-4 rounded">
-        <MapContainer
+        <Map
           key={`${center[0]}-${center[1]}`}
           style={{ height: '400px !important', width: '100% !important' }}
           center={center}
@@ -63,10 +79,18 @@ function EventsByLocation() {
             icon={customMarkerIcon}
           >
             <Popup>
-              <h1>You are here!</h1>
+              <div>
+                <p className="fs-5 fw-normal mb-3 ">24/06/2022 -26/06/2022</p>
+                <div className="d-flex align-items-baseline">
+                  <FontAwesomeIcon icon={solid('location-dot')} className="text-primary me-2" size="lg" />
+                  <span className="fs-3 fw-normal mb-2">1 Main St, New York, NY USA</span>
+                </div>
+                <h1 className="h3 fw-bold">Escape from a House of Horror - A Diane Sawyer Special Event</h1>
+                <Button className="bg-transparent text-primary border-0 shadow-none p-0">View event</Button>
+              </div>
             </Popup>
           </Marker>
-        </MapContainer>
+        </Map>
 
         <Button
           className="d-flex mx-auto bg-transparent shadow-none border-0 fs-3 text-light mt-3"

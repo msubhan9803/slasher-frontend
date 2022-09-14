2ed6c44d6c9153d911dfdb214b03ab1793dbd546
Eric O <eric@ejdigitalmedia.com>
2022-09-14 02:48:11 -0400

    Minor updates

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,2 @@
-const apiUrl = process.env.REACT_APP_API_URL;
-export default apiUrl;
+// eslint-disable-next-line import/prefer-default-export
+export const apiUrl = process.env.REACT_APP_API_URL;
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,32 +1,32 @@
-import apiUrl from './constants';
+import { apiUrl } from './constants';
 
 export default function signIn(emailOrUsername: string, password: string) {
   const body = {
     emailOrUsername,
     password,
-    device_id: 1,
-    device_token: 'wewewewew',
-    device_type: 'mobile',
-    app_version: 1.0,
-    device_version: 2.9,
+    device_id: '',
+    device_token: '',
+    device_type: 'browser',
+    app_version: `web-${process.env.REACT_APP_VERSION}`,
+    device_version: window.navigator.userAgent,
   };
-  fetch(`${apiUrl}users/sign-in`, {
+
+  return fetch(`${apiUrl}users/sign-in`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body),
-  })
-    .then((response) => response.json());
+  }).then((response) => response.json());
 }
 
 export function register(
   firstName: string,
   userName: string,
-  email:string,
-  password:string,
-  passwordConfirmation:string,
-  securityQuestion:string,
-  securityAnswer:string,
-  dob:string,
+  email: string,
+  password: string,
+  passwordConfirmation: string,
+  securityQuestion: string,
+  securityAnswer: string,
+  dob: string,
 ) {
   const body = {
     firstName,
@@ -38,10 +38,9 @@ export function register(
     securityAnswer,
     dob,
   };
-  fetch(`${apiUrl}users/register`, {
+  return fetch(`${apiUrl}users/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body),
-  })
-    .then((response) => response.json());
+  }).then((response) => response.json());
 }

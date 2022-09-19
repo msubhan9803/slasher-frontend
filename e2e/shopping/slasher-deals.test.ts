4ffb5dad675b142c29d7cecdc95eba7b3c9b6f1b
Ronak Diyora <ronakdiyora@outlook.com>
2022-09-19 22:21:17 +0530

    implement mock test api and add it to respective authenticated route

--- e2e/shopping/slasher-deals.test.ts
+++ e2e/shopping/slasher-deals.test.ts
@@ -1,10 +1,23 @@
 import { test, expect } from '@playwright/test';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import Cookies from 'js-cookie';
 
 const pagePath = '/shopping/slasher-deals';
 
 test.describe(pagePath, () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto(pagePath);
+    const mock = new MockAdapter(axios);
+    // Mock any GET request to /users
+    // arguments for reply are (status, data, headers)
+    mock.onGet('/users').reply(200, {
+      users: [{ id: 1, name: 'John Smith' }],
+    });
+
+    axios.get('/users').then(async () => {
+      Cookies.set('sessionToken', 'mockToken');
+      await page.goto(pagePath);
+    });
   });
 
   test('should display the expected content', async ({ page }) => {

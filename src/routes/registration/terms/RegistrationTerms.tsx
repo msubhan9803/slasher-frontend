d6e7f745440ad90c114fa240ad5df3b871426e26
Ronak Diyora <ronakdiyora@outlook.com>
2022-09-19 16:51:44 +0530

    - add checkbox in registartion terms

--- src/routes/registration/terms/RegistrationTerms.tsx
+++ src/routes/registration/terms/RegistrationTerms.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Alert,
   Col,
   Row,
 } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import CommunityStandardsAndRules from '../../../components/terms-and-policies/CommunityStandardsAndRules';
 import EndUserLicenseAgreement from '../../../components/terms-and-policies/EndUserLicenseAgreement';
 import PrivacyPolicy from '../../../components/terms-and-policies/PrivacyPolicy';
 import TermsAndConditions from '../../../components/terms-and-policies/TermsAndConditions';
+import RoundButton from '../../../components/ui/RoundButton';
 import RoundButtonLink from '../../../components/ui/RoundButtonLink';
 import RegistrationPageWrapper from '../components/RegistrationPageWrapper';
 
@@ -15,6 +18,11 @@ interface Props {
 }
 
 function RegistrationTerms({ activeStep }: Props) {
+  const navigate = useNavigate();
+  const [checked, setChecked] = useState(false);
+  const [isAlert, setAlert] = useState(false);
+
+  const handleSignup = () => (checked ? navigate('/registration/final') : setAlert(true));
   return (
     <RegistrationPageWrapper activeStep={activeStep}>
       <p className="fs-3 mb-5">
@@ -25,12 +33,37 @@ function RegistrationTerms({ activeStep }: Props) {
       <PrivacyPolicy className="my-5" />
       <EndUserLicenseAgreement className="my-5" />
       <CommunityStandardsAndRules className="my-5" />
-      <h2 className="my-4 border-bottom pb-2">I agree</h2>
+      <div className="my-4 border-bottom pb-2">
+        <label htmlFor="term-checkbox" className="h2">
+          <input
+            id="term-checkbox"
+            type="checkbox"
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+            className="me-2"
+          />
+          I agree to these terms
+        </label>
+      </div>
+      {/* <h2 className="my-4 border-bottom pb-2">I agree</h2> */}
       <p>
         By clicking Sign up, you agree that you are at least 17 years of age, and that you agree
         with our Terms and Conditions, Privacy Policy, End User License Agreement, and Community
         Standards.
       </p>
+      {/* <label htmlFor="term">
+        <input
+          id="term"
+          type="checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+          className="me-2"
+        />
+        I agree to these terms
+      </label> */}
+      <div className="mt-2">
+        {isAlert && <Alert variant="info">You must agree to the terms if you want to sign up.</Alert>}
+      </div>
       <Row className="justify-content-center my-5">
         <Col sm={4} md={3} className="mb-sm-0 mb-3">
           <RoundButtonLink to="/registration/security" className="w-100" variant="secondary">
@@ -38,9 +71,9 @@ function RegistrationTerms({ activeStep }: Props) {
           </RoundButtonLink>
         </Col>
         <Col sm={4} md={3}>
-          <RoundButtonLink to="/registration/final" className="w-100" variant="primary">
+          <RoundButton className="w-100" onClick={handleSignup}>
             Sign up
-          </RoundButtonLink>
+          </RoundButton>
         </Col>
       </Row>
     </RegistrationPageWrapper>

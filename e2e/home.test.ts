68d1efbe7c50ac6b7019feec951f8fba6fe0b65f
Eric O <eric@ejdigitalmedia.com>
2022-09-21 00:33:40 -0400

    Move beforeEach inside describe in home.test.ts

--- e2e/home.test.ts
+++ e2e/home.test.ts
@@ -3,12 +3,12 @@ import { performSignIn } from './e2e-test-helpers';
 
 const pagePath = '/home';
 
-test.beforeEach(async ({ page }) => {
-  await performSignIn(page);
-  await page.goto(pagePath);
-});
-
 test.describe(pagePath, () => {
+  test.beforeEach(async ({ page }) => {
+    await performSignIn(page);
+    await page.goto(pagePath);
+  });
+
   test('should display the expected content', async ({ page }) => {
     await expect(page.locator('main')).toHaveText(/Suggested friends/);
   });

270df730870bcd4fb0b08f8c7b92260d2494eeee
Ronak Diyora <ronakdiyora@outlook.com>
2022-09-23 21:29:00 +0530

    get userid and token inside the function

--- src/api/onboarding.ts
+++ src/api/onboarding.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { apiUrl } from './constants';
 
-const userId = Cookies.get('userId');
-const token = Cookies.get('sessionToken');
 // eslint-disable-next-line import/prefer-default-export
 export async function onboardingAboutMe(message: string) {
+  const userId = Cookies.get('userId');
+  const token = Cookies.get('sessionToken');
   return axios.patch(
     `${apiUrl}/users/${userId}`,
     {

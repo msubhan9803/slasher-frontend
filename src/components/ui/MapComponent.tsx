6edb0730f17953c022224a75dacbac264018dbc2
Ronak Diyora <ronakdiyora@outlook.com>
2022-09-26 18:42:54 +0530

    make common map component design

---
+++ src/components/ui/MapComponent.tsx
@@ -0,0 +1,82 @@
+import React, { useRef } from 'react';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  TileLayer, Marker, Popup, MapContainer,
+} from 'react-leaflet';
+import {
+  Button, Col, Form, Row,
+} from 'react-bootstrap';
+import styled from 'styled-components';
+import Leaflet from 'leaflet';
+import ReactDOMServer from 'react-dom/server';
+
+const Map = styled(MapContainer)`
+  height: 400px !important;
+  width: 100% !important;
+  z-index:0;
+  .leaflet-div-icon {
+    background: transparent !important;
+    border: none !important;
+  }
+  .leaflet-popup-content{
+    margin: 13px 20px 13px 16px;
+  }
+  .leaflet-popup {
+    bottom : 0 !important;
+  }
+`;
+
+const iconHTML = ReactDOMServer.renderToString(<FontAwesomeIcon icon={solid('location-dot')} size="3x" className="text-primary" />);
+const customMarkerIcon = new Leaflet.DivIcon({
+  html: iconHTML,
+  iconAnchor: [12, 5], // [left/right, top/bottom]
+});
+
+function MapComponent({ center, ZOOM_LEVEL }: any) {
+  const mapRef = useRef() as any;
+  return (
+    <div>
+      <Row className="align-items-center">
+        <Col sm={4} md={3} lg={6} xl={4} className="mb-4">
+          <Button className="w-100">Detect my location</Button>
+        </Col>
+        <Col sm={8} md={9} lg={6} xl={8} className="mb-4">
+          <Form.Control
+            placeholder="Enter a location…"
+            className="fs-5"
+          />
+        </Col>
+      </Row>
+      <Map
+        key={`${center[0]}-${center[1]}`}
+        center={center}
+        zoom={ZOOM_LEVEL}
+        ref={mapRef}
+      >
+        <TileLayer
+          url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=SLcHU7I1RZjHoCZGIbr6"
+          attribution={'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
+        />
+        <Marker
+          position={center}
+          icon={customMarkerIcon}
+        >
+          <Popup>
+            <div>
+              <p className="fs-5 fw-normal mb-3 ">24/06/2022 -26/06/2022</p>
+              <div className="d-flex align-items-baseline">
+                <FontAwesomeIcon icon={solid('location-dot')} className="text-primary me-2" size="lg" />
+                <span className="fs-3 fw-normal mb-2">1 Main St, New York, NY USA</span>
+              </div>
+              <h1 className="h3 fw-bold">Escape from a House of Horror - A Diane Sawyer Special Event</h1>
+              <Button className="bg-transparent text-primary border-0 shadow-none p-0">View event</Button>
+            </div>
+          </Popup>
+        </Marker>
+      </Map>
+    </div>
+  );
+}
+
+export default MapComponent;

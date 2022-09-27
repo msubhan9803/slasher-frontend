579976f8d36fc5a07314c160052409de6aa385b2
Eric O <eric@ejdigitalmedia.com>
2022-09-27 03:08:43 -0400

    Make sign-in page e2e redirect test specifically look for expected redirect page url in browser

--- e2e/sign-in.test.ts
+++ e2e/sign-in.test.ts
@@ -4,8 +4,14 @@ import { performSignIn } from './e2e-test-helpers';
 const pagePath = '/sign-in';
 
 test.describe(pagePath, () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(pagePath);
+  test.describe('for a user who is not currently signed in', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto(pagePath);
+    });
+    test('should display the expected content', async ({ page }) => {
+      await expect(page.locator('main')).toHaveText(/Sign in/);
+      await expect(page.locator('main')).toHaveText(/Forgot your password\?/);
+    });
   });
 
   test.describe('for a signed-in user', () => {
@@ -13,15 +19,10 @@ test.describe(pagePath, () => {
       await performSignIn(page);
       await page.goto(pagePath);
     });
-    test('should redirect to the /home page', async ({ page }) => {
-      await expect(page.locator('main')).toHaveText(/Suggested friends/);
-    });
-  });
-
-  test.describe('for a user who is not currently signed in', () => {
-    test('should display the expected content', async ({ page }) => {
-      await expect(page.locator('main')).toHaveText(/Sign in/);
-      await expect(page.locator('main')).toHaveText(/Forgot your password\?/);
+    test('should redirect to the /home page', async ({ page, baseURL }) => {
+      const homeUrl = `${baseURL}/home`;
+      await page.waitForURL(homeUrl);
+      await expect(page.url()).toBe(homeUrl);
     });
   });
 });

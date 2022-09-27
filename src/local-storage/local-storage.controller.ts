275d9cadc482bda18f56c35fda3929775149b701
Eric O <eric@ejdigitalmedia.com>
2022-09-27 19:47:56 -0400

    Update dto name to match controller method where it is used

--- src/local-storage/local-storage.controller.ts
+++ src/local-storage/local-storage.controller.ts
@@ -5,7 +5,7 @@ import { Response } from 'express';
 import { createReadStream } from 'fs';
 import { fileNameToMimeType } from '../utils/mime-utils';
 import { defaultQueryDtoValidationPipeOptions } from '../utils/validation-utils';
-import { LocationParamDto } from './dto/location-param.dto';
+import { GetFileDto } from './dto/getFile.dto';
 import { LocalStorageService } from './providers/local-storage.service';
 
 @Controller('local-storage')
@@ -15,10 +15,10 @@ export class LocalStorageController {
   @Get(':location(*)')
   async getFile(
     @Param(new ValidationPipe(defaultQueryDtoValidationPipeOptions))
-    param: LocationParamDto,
+    params: GetFileDto,
     @Res() res: Response,
   ) {
-    const filePath = await this.localStorageService.getLocalFilePath(`/${param.location}`);
+    const filePath = await this.localStorageService.getLocalFilePath(`/${params.location}`);
     if (!filePath) throw new HttpException('File not found', HttpStatus.NOT_FOUND);
     res.setHeader('Content-Type', fileNameToMimeType(filePath));
     const file = createReadStream(filePath);

2ff88b8052540dc2ce4c887bc17912bcd92db542
Eric O <eric@ejdigitalmedia.com>
2022-10-03 01:28:55 -0400

    Add NotificationsGateway and work-in-progress code for emitting event from controller and listening on client socket connection

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,14 +1,18 @@
 import {
-  Controller, Get, Req,
+  Controller, Get, Post, Req,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { pick } from '../utils/object-utils';
 import { getUserFromRequest } from '../utils/request-utils';
+import { NotificationsGateway } from './providers/notifications.gateway';
 import { NotificationsService } from './providers/notifications.service';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) { }
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private readonly notificationsGateway: NotificationsGateway,
+  ) { }
 
   @Get()
   async index(@Req() request: Request) {
@@ -17,5 +21,11 @@ export class NotificationsController {
     return (await this.notificationsService.findAllByUserId(user._id)).map(
       (notification) => pick(notification, ['notificationMsg', 'createdAt']),
     );
+  }
+
+  @Post('test')
+  async socketMessageEmitTest() {
+    this.notificationsGateway.server.emit('hello', 'world');
+    return 'test';
   }
 }

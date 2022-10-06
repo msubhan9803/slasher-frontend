180419684809d29dabd913a37ee8a915f733f4a9
Eric O <eric@ejdigitalmedia.com>
2022-10-06 00:54:54 -0400

    Add RssFeedProvider schema

---
+++ src/schemas/rssFeedProvider/rssFeedProvider.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { RssFeedProviderUnusedFields } from './rssFeedProvider.unused-fields';
+
+@Schema({ timestamps: true })
+export class RssFeedProvider extends RssFeedProviderUnusedFields {
+  /***********
+   * Fields *
+   ***********/
+
+  readonly _id: mongoose.Schema.Types.ObjectId;
+
+  @Prop()
+  createdAt: Date; // automatically populated on save by Mongoose {timestamps: true} configuration
+
+  @Prop()
+  updatedAt: Date; // automatically populated on save by Mongoose {timestamps: true} configuration
+
+  /***********
+   * Methods *
+   ***********/
+
+  constructor(options?: Partial<RssFeedProvider>) {
+    super();
+    if (!options) {
+      return;
+    }
+    Object.keys(options).forEach((key) => {
+      this[key] = options[key];
+    });
+  }
+}
+
+export const RssFeedProviderSchema = SchemaFactory.createForClass(RssFeedProvider);
+
+export type RssFeedProviderDocument = RssFeedProvider & Document;
---
+++ src/schemas/rssFeedProvider/rssFeedProvider.unused-fields.ts
@@ -0,0 +1,68 @@
+import { Prop } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { ReportUserSchema, ReportUser } from '../shared/reportUser.schema';
+import { RssFeedProviderActiveStatus, RssFeedProviderAutoFollow, RssFeedProviderDeletionStatus } from './rssFeedProvider.enums';
+
+export class RssFeedProviderUnusedFields {
+  @Prop({ default: null })
+  title: string;
+
+  @Prop({ default: null })
+  logo: string;
+
+  @Prop({ default: null })
+  feed_url: string;
+
+  @Prop({ default: RssFeedProviderAutoFollow.No })
+  auto_follow: RssFeedProviderAutoFollow;
+
+  @Prop({ default: 0 })
+  preview_count: number;
+
+  // NOT USED
+  @Prop({
+    enum: [
+      RssFeedProviderActiveStatus.Inactive,
+      RssFeedProviderActiveStatus.Active,
+      RssFeedProviderActiveStatus.Deactivated,
+    ],
+    default: RssFeedProviderActiveStatus.Inactive,
+  })
+  status: RssFeedProviderActiveStatus;
+
+  // NOT USED
+  @Prop({
+    enum: [
+      RssFeedProviderDeletionStatus.NotDeleted,
+      RssFeedProviderDeletionStatus.Deleted,
+    ],
+    default: RssFeedProviderDeletionStatus.NotDeleted,
+  })
+  deleted: RssFeedProviderDeletionStatus;
+
+  @Prop({ default: 0 })
+  follower_count: number;
+
+  @Prop({ default: null })
+  description: string;
+
+  // NOT USED
+  @Prop({ default: null, ref: 'users' })
+  createdBy: mongoose.Schema.Types.ObjectId;
+
+  // NOT USED
+  @Prop({ type: [ReportUserSchema] })
+  reportUsers: ReportUser[];
+
+  // NOT USED
+  // This might be an array of ObjectIds of the users. Not 100% clear, since all records in prod
+  // db currently hold an empty array.
+  @Prop({ type: Array, default: [] })
+  hideUsers: mongoose.Schema.Types.ObjectId[];
+
+  // NOT USED
+  // This might be an array of ObjectIds of the users. Not 100% clear, since all records in prod
+  // db currently hold an empty array.
+  @Prop({ type: Array, default: [] })
+  shareUsers: mongoose.Schema.Types.ObjectId[];
+}

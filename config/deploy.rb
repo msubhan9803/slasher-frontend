b95410988a5d44795000eaf664a3396c13c19c3c
Eric O <eric@ejdigitalmedia.com>
2022-10-09 21:37:42 -0400

    Deployment: Slasher-specific capistrano setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,40 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+#set :log_level, :debug
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :repo_url, "git@bitbucket.org:slasherapp/slasher-web-new.git"
+set :application, "slasher"
+set :deploy_to, "/var/www/#{fetch(:application)}_#{fetch(:stage)}"
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
+set :ssh_options, {
+  #keys: %w(~/.ssh/id_rsa),
+  forward_agent: true, # To make this work, make sure to run: ssh-add ~/.ssh/id_rsa
+  auth_methods: %w(publickey)
+}
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, '.env'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+namespace :deploy do
+  desc 'Build application'
+  task :build_app do
+    on roles(:app) do
+      within release_path do
+        execute :npm, 'ci'
+        execute :npm, 'run build'
+        # After build, we no longer need the dev dependencies, so we'll remove them
+        execute :npm, 'prune --omit=dev'
+      end
+    end
+  end
+
+  before :publishing, :build_app
+end

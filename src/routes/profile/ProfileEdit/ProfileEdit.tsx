eb9c540a4794f7f317f2c7a194f50654ceb15253
Avadh Ladani <avadh.ladani353@gmail.com>
2022-10-14 15:45:13 +0530

    all profile detail api integrate

--- src/routes/profile/ProfileEdit/ProfileEdit.tsx
+++ src/routes/profile/ProfileEdit/ProfileEdit.tsx
@@ -1,14 +1,85 @@
-import React, { ChangeEvent, useState } from 'react';
+/* eslint-disable max-lines */
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Col, Form, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import {
+  getUserProfileDetail,
+  uploadUserCoverImage, uploadUserProfileImage, updateUserProfile,
+} from '../../../api/users';
 import AuthenticatedPageWrapper from '../../../components/layout/main-site-wrapper/authenticated/AuthenticatedPageWrapper';
+import ErrorMessageList from '../../../components/ui/ErrorMessageList';
 import PhotoUploadInput from '../../../components/ui/PhotoUploadInput';
 import RoundButton from '../../../components/ui/RoundButton';
 
+interface UserDataProps {
+  userName: string;
+  firstName: string;
+  email: string;
+  id: string;
+  profilePic: string;
+  coverPhoto: string;
+}
+
 function ProfileEdit() {
-  const userData = {
-    id: 1, name: 'Aly Khan', username: 'aly-khan', email: 'aly@slasher.tv',
+  const [updateUserData, setUpdateUserData] = useState<UserDataProps>({
+    userName: '',
+    firstName: '',
+    email: '',
+    id: '',
+    profilePic: '',
+    coverPhoto: '',
+  });
+  const [errorMessage, setErrorMessages] = useState<string[]>();
+  const [profilePhoto, setProfilePhoto] = useState<any>();
+  const [coverPhoto, setCoverPhoto] = useState<any>();
+  const params = useParams();
+
+  useEffect(() => {
+    if (params && params.userName) {
+      getUserProfileDetail(params.userName)
+        .then((res) => setUpdateUserData(res.data))
+        .catch((error) => setErrorMessages(error));
+    }
+  }, [params]);
+
+  const updateProfile = async () => {
+    let errorList: string[] = [];
+
+    if (profilePhoto) {
+      try {
+        await uploadUserProfileImage(profilePhoto);
+      } catch (requestError: any) {
+        errorList = errorList.concat(requestError.response.data.message);
+      }
+    }
+
+    if (coverPhoto) {
+      try {
+        await uploadUserCoverImage(coverPhoto);
+      } catch (requestError: any) {
+        errorList = errorList.concat(requestError.response.data.message);
+      }
+    }
+
+    try {
+      await updateUserProfile(
+        updateUserData.userName,
+        updateUserData.firstName,
+        updateUserData.email,
+        updateUserData.id,
+      );
+    } catch (requestError: any) {
+      errorList = errorList.concat(requestError.response.data.message);
+    }
+    setErrorMessages(errorList);
   };
-  const [updateUserData, setUpdateUserData] = useState(userData);
+
+  const handleChange = (value: string, key: string) => {
+    const userTempData = { ...updateUserData };
+    (userTempData as any)[key] = value;
+    setUpdateUserData(userTempData);
+  };
+
   return (
     <AuthenticatedPageWrapper rightSidebarType="profile-self">
       <Form>
@@ -20,9 +91,8 @@ function ProfileEdit() {
                   className="mx-auto mx-md-0 me-md-3"
                   height="10rem"
                   variant="outline"
-                // onChange={(file) => {
-                //   // TODO
-                // }}
+                  imagePreview={updateUserData.profilePic}
+                  onChange={(file) => { setProfilePhoto(file); }}
                 />
                 <div className="text-center text-md-start mt-4 mt-md-0">
                   <h1 className="h3 mb-2 fw-bold">Change profile photo</h1>
@@ -43,9 +113,8 @@ function ProfileEdit() {
                   className="mx-auto mx-md-0 me-md-3"
                   height="10rem"
                   variant="outline"
-                // onChange={(file) => {
-                //   // TODO
-                // }}
+                  imagePreview={updateUserData.coverPhoto}
+                  onChange={(file) => { setCoverPhoto(file); }}
                 />
                 <div className="text-center text-md-start mt-4 mt-md-0">
                   <h1 className="h3 mb-2 fw-bold">Change cover photo</h1>
@@ -68,7 +137,15 @@ function ProfileEdit() {
             <Col md={6}>
               <Form.Group className="mb-4">
                 <Form.Label className="h3">Name</Form.Label>
-                <Form.Control type="text" placeholder="Name" value={updateUserData.name} onChange={(changeData: ChangeEvent<HTMLInputElement>) => setUpdateUserData({ ...updateUserData, name: changeData.target.value })} className="my-3 fs-5" />
+                <Form.Control
+                  type="text"
+                  placeholder="Name"
+                  value={updateUserData.firstName || ''}
+                  onChange={
+                    (changeData: ChangeEvent<HTMLInputElement>) => handleChange(changeData.target.value, 'firstName')
+                  }
+                  className="my-3 fs-5"
+                />
                 <div className="d-flex pe-5">
                   <Form.Text className="text-muted fs-4">
                     This is how your first name will appear in your profile.
@@ -79,7 +156,15 @@ function ProfileEdit() {
             <Col md={6}>
               <Form.Group className="mb-4">
                 <Form.Label className="h3">Username</Form.Label>
-                <Form.Control type="text" placeholder="Username" value={updateUserData.username} onChange={(changeData: ChangeEvent<HTMLInputElement>) => setUpdateUserData({ ...updateUserData, username: changeData.target.value })} className="my-3 fs-5" />
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  value={updateUserData.userName || ''}
+                  onChange={
+                    (changeData: ChangeEvent<HTMLInputElement>) => handleChange(changeData.target.value, 'userName')
+                  }
+                  className="my-3 fs-5"
+                />
                 <Form.Text className="text-muted fs-4">
                   This is how people will see you post and comment on Slasher.
                 </Form.Text>
@@ -90,7 +175,15 @@ function ProfileEdit() {
             <Col md={6}>
               <Form.Group className="mb-4">
                 <Form.Label className="h3">Email address</Form.Label>
-                <Form.Control type="email" placeholder="Email" value={updateUserData.email} onChange={(changeData: ChangeEvent<HTMLInputElement>) => setUpdateUserData({ ...updateUserData, email: changeData.target.value })} className="my-3 fs-5" />
+                <Form.Control
+                  type="email"
+                  placeholder="Email"
+                  value={updateUserData.email || ''}
+                  onChange={
+                    (changeData: ChangeEvent<HTMLInputElement>) => handleChange(changeData.target.value, 'email')
+                  }
+                  className="my-3 fs-5"
+                />
                 <Form.Text className="text-muted fs-4">
                   In order to edit your email address, we will ask your
                   security question.
@@ -107,9 +200,14 @@ function ProfileEdit() {
               </Form.Group>
             </Col>
           </Row>
+          {errorMessage && errorMessage.length > 0 && (
+            <div className="mt-3 text-start">
+              <ErrorMessageList errorMessages={errorMessage} className="m-0" />
+            </div>
+          )}
           <Row className="mt-2">
             <Col md={3} lg={4} xl={3}>
-              <RoundButton className="py-2 w-100  fs-3 fw-bold">
+              <RoundButton className="py-2 w-100  fs-3 fw-bold" onClick={updateProfile}>
                 Update profile
               </RoundButton>
             </Col>
@@ -119,5 +217,4 @@ function ProfileEdit() {
     </AuthenticatedPageWrapper>
   );
 }
-
 export default ProfileEdit;

3403c53c99e8a298a7df6e441916ed965087ce20
Avadh Ladani <avadh.ladani353@gmail.com>
2022-11-16 19:11:23 +0530

    add support to remove blocked user

--- src/routes/account/AccountBlockedUser/AccountBlockedUser.tsx
+++ src/routes/account/AccountBlockedUser/AccountBlockedUser.tsx
@@ -1,82 +1,130 @@
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
-import styled from 'styled-components';
-import AuthenticatedPageWrapper from '../../../components/layout/main-site-wrapper/authenticated/AuthenticatedPageWrapper';
-import AccountHeader from '../AccountHeader';
-import CustomPopover from '../../../components/ui/CustomPopover';
-import UserCircleImage from '../../../components/ui/UserCircleImage';
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import styled from "styled-components";
+import InfiniteScroll from "react-infinite-scroller";
+import Cookies from "js-cookie";
+import ErrorMessageList from "../../../components/ui/ErrorMessageList";
+import AuthenticatedPageWrapper from "../../../components/layout/main-site-wrapper/authenticated/AuthenticatedPageWrapper";
+import AccountHeader from "../AccountHeader";
+import CustomPopover from "../../../components/ui/CustomPopover";
+import UserCircleImage from "../../../components/ui/UserCircleImage";
+import { blockedUsers, removeBlockedUsers } from "../../../api/blocks";
+
+interface BlockUsers {
+  /* eslint no-underscore-dangle: 0 */
+  _id: string;
+  firstName: string;
+  userName: string;
+  profilePic: string;
+}
 
 const Container = styled.div`
   @media (min-width: 992px) {
-    background: #1F1F1F;
+    background: #1f1f1f;
   }
 `;
-const friendList = [
-  {
-    id: 1, imageUrl: 'https://i.pravatar.cc/300?img=19', name: 'Maureen Biologist', email: '@maureenbio122',
-  },
-  {
-    id: 2, imageUrl: 'https://i.pravatar.cc/300?img=12', name: 'Benjamin Lucas', email: '@maureenbio122',
-  },
-  {
-    id: 3, imageUrl: 'https://i.pravatar.cc/300?img=19', name: 'Theodore Henry', email: '@maureenbio122',
-  },
-  {
-    id: 4, imageUrl: 'https://i.pravatar.cc/300?img=12', name: 'Maureen Biologist', email: '@maureenbio122',
-  },
-  {
-    id: 5, imageUrl: 'https://i.pravatar.cc/300?img=32', name: 'Alexander Daniel', email: '@maureenbio122',
-  },
-  {
-    id: 6, imageUrl: 'https://i.pravatar.cc/300?img=12', name: 'Michael Mason', email: '@maureenbio122',
-  },
-  {
-    id: 7, imageUrl: 'https://i.pravatar.cc/300?img=13', name: 'Sebastian', email: '@maureenbio122',
-  },
-  {
-    id: 8, imageUrl: 'https://i.pravatar.cc/300?img=12', name: 'Samuel Jacob', email: '@maureenbio122',
-  },
-  {
-    id: 9, imageUrl: 'https://i.pravatar.cc/300?img=12', name: 'Aiden John', email: '@maureenbio122',
-  },
-  {
-    id: 10, imageUrl: 'https://i.pravatar.cc/300?img=17', name: 'Joseph Wyatt', email: '@maureenbio122',
-  },
-  {
-    id: 11, imageUrl: 'https://i.pravatar.cc/300?img=32', name: 'Hudson Grayson', email: '@maureenbio122',
-  },
-  {
-    id: 12, imageUrl: 'https://i.pravatar.cc/300?img=20', name: 'Gabriel Jayden', email: '@maureenbio122',
-  },
-];
-const PopoverOption = ['Unblock user'];
+const PopoverOption = ["Unblock user"];
 function AccountBlockedUser() {
+  const [blockUsersList, setBlockUsersList] = useState<BlockUsers[]>([]);
+  const [noMoreData, setNoMoreData] = useState<Boolean>(false);
+  const [page, setPage] = useState<number>(0);
+  const [errorMessage, setErrorMessage] = useState<string[]>();
+
+  const getBlockedUserList = (page: number) => {
+    blockedUsers(page)
+      .then((res) => {
+        setBlockUsersList(res.data);
+        setPage(page + 1);
+      })
+      .catch((error) => setErrorMessage(error.response.data.message));
+  };
+
+  useEffect(() => {
+    getBlockedUserList(page);
+  }, []);
+
+  const fetchMoreBlockUsersList = () => {
+    if (page > 0) {
+      blockedUsers(page).then((res) => {
+        setBlockUsersList((prev: any) => [...prev, ...res.data]);
+        setPage(page + 1);
+        if (res.data.length === 0) {
+          setNoMoreData(true);
+        }
+      });
+    }
+  };
+
+  const renderNoMoreDataMessage = () => (
+    <p className="text-center">
+      {blockUsersList.length === 0
+        ? "No blocked users at the moment."
+        : "No more block user"}
+    </p>
+  );
+
+  const removeBlockUser = (userId: string) => {
+    removeBlockedUsers(userId).then((res) => {
+      console.log("remove user", res);
+      setTimeout(() => {
+        getBlockedUserList(0);
+      }, 2000);
+      setPage(0);
+    });
+  };
+
   return (
     <AuthenticatedPageWrapper rightSidebarType="profile-self">
       <AccountHeader tabKey="blocked-users" />
       <div className="bg-mobile-transparent border-0 rounded-3 bg-dark mb-0 p-md-3 pb-md-1 my-3">
-        <Row>
-          {friendList.map((friend) => (
-            <Col xs sm={6} md={4} lg={6} xl={4} key={friend.id}>
-              <Container className="d-flex p-2 justify-content-between pe-2 w-100 rounded mb-3">
-                <div>
-                  <div className="d-flex align-items-center">
+        <InfiniteScroll
+          pageStart={0}
+          initialLoad={false}
+          loadMore={fetchMoreBlockUsersList}
+          hasMore={!noMoreData}
+        >
+          <Row>
+            {blockUsersList &&
+              blockUsersList.length > 0 &&
+              blockUsersList.map((blockUser) => (
+                <Col xs sm={6} md={4} lg={6} xl={4} key={blockUser._id}>
+                  <Container className="d-flex p-2 justify-content-between pe-2 w-100 rounded mb-3">
                     <div>
-                      <UserCircleImage src={friend.imageUrl} className="me-2" />
+                      <div className="d-flex align-items-center">
+                        <div>
+                          <UserCircleImage
+                            src={blockUser.profilePic}
+                            className="me-2"
+                          />
+                        </div>
+                        <div>
+                          <h1 className="h3 mb-0">{blockUser.firstName}</h1>
+                          <p className="fs-6 mb-0 text-light">
+                            {blockUser.userName}
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                    <div>
-                      <h1 className="h3 mb-0">{friend.name}</h1>
-                      <p className="fs-6 mb-0 text-light">{friend.email}</p>
+                    <div className="d-flex align-self-center">
+                      <CustomPopover
+                        popoverOptions={PopoverOption}
+                        onPopoverClick={() => {
+                          console.log("blockUser =", blockUser._id);
+                          removeBlockUser(blockUser._id);
+                        }}
+                      />
                     </div>
-                  </div>
-                </div>
-                <div className="d-flex align-self-center">
-                  <CustomPopover popoverOptions={PopoverOption} onPopoverClick={() => { }} />
-                </div>
-              </Container>
-            </Col>
-          ))}
-        </Row>
+                  </Container>
+                </Col>
+              ))}
+          </Row>
+        </InfiniteScroll>
+        {noMoreData && renderNoMoreDataMessage()}
+        {errorMessage && errorMessage.length > 0 && (
+          <div className="mt-3 text-start">
+            <ErrorMessageList errorMessages={errorMessage} className="m-0" />
+          </div>
+        )}
       </div>
     </AuthenticatedPageWrapper>
   );

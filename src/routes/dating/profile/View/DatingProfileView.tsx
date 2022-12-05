ae82c5be8b9121e76d48696fef75b0e3d8b8920f
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-06 05:02:13 +0530

    Setup complete screen setup, with mobile responsiveness.

--- src/routes/dating/profile/View/DatingProfileView.tsx
+++ src/routes/dating/profile/View/DatingProfileView.tsx
@@ -1,48 +1,169 @@
 import React, { useState } from 'react';
-import { Col } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import RoundButton from '../../../../components/ui/RoundButton';
+import mapPinIcon from '../../../../images/dating/dating-map-pin.svg';
+import threeDotMenu from '../../../../images/dating/three_dots_icon.png';
+import profileImg1 from '../../../../images/dating/sample_profile/sample_profile1.png';
+import profileImg2 from '../../../../images/dating/sample_profile/sample_profile2.png';
+import profileImg3 from '../../../../images/dating/sample_profile/sample_profile3.png';
+import profileImg4 from '../../../../images/dating/sample_profile/sample_profile4.png';
+import profileImg5 from '../../../../images/dating/sample_profile/sample_profile5.png';
+import profileImg6 from '../../../../images/dating/sample_profile/sample_profile6.png';
+import profileImg7 from '../../../../images/dating/sample_profile/sample_profile7.png';
 import DatingPageWrapper from '../../components/DatingPageWrapper';
-// import { useParams } from 'react-router-dom';
+import ProfileMobileView from './ProfileMobileView';
+import {
+  Gender, Heading, HeroProfileImg, Section, SubHeading, Title,
+} from './styledUtilsProfileView';
 
 const initial = {
-  nameAndAge: 'Elizabeth, 28',
-  gender: 'female',
-  distance: '24 mi/39 km',
+  nameAndAge: 'Elizabeth 28',
+  gender: 'Female',
+  distance: '24mi / 39km',
   aboutMe: 'By day I am a financial advisor, at night I am a beer drinking music fan who loves attending live music events. Up for all and any adventures and wants someone to complement my life, not complicate it. Hit me up if you feel the same. Fun loving barista who enjoys bike riding, hiking, and eating pepperoni pizza. I have two dogs who I will love more than you and a collection of band t-shirts that will surprise you. I’ve always known the secret to happiness is gratitude. I’m still surprised and humbled by how much I have in my life to be grateful for!',
   height: '5\'4"/164 cm',
   bodyType: 'Tall',
   tattoos: 'yes',
+  lookingFor: 'Men',
+  relationshipState: 'Open relationship',
+  sexualOrientation: 'Bisexual',
+  parentalStatus: 'No children',
+  employment: 'Self-employed',
+  educationLevel: 'Graduate',
+  drinking: 'No',
+  smoking: 'Casual Smoker',
+  religion: 'Catholic',
+  interests: ['Photography', 'Swimming', 'Anime', 'Dancing', 'Cooking', 'Board games'],
 };
 
 function DatingProfileView() {
-//   const params = useParams();
-  // console.log('params.datingUserId?', params.datingUserId);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const params = useParams();
+  // IMPORATANT NOTE:
+  // We must use `params.datingUserId` to fetch individual profile details in the
+  // `useEffect` hook in this component when we integrate server api to make this
+  // component portable across popup screen i.e.,
+  // "Dating Deck - View Profile, Viewer - (not matched), Popup"
+
   const [state] = useState(initial);
 
   return (
     <DatingPageWrapper>
-      DatingProfileDetails
-      <div>{state.nameAndAge}</div>
-      <div>{state.gender}</div>
-      <div>{state.distance}</div>
+      <Section>
+        {/* Desktop Only Section. *LEARN: Hide by default and show on xxl and wider screens. */}
+        <div className="d-none d-xxl-block">
+          <div className="d-flex">
+            {/* Left COLUMN */}
+            <HeroProfileImg><img className="rounded-3 d-block m-auto" src={profileImg1} alt="Profile Here" /></HeroProfileImg>
+            {/* Right COLUMN */}
+            <div className="w-100 d-flex flex-column justify-content-between">
+              {/* Upper */}
+              <div className="d-flex justify-content-between">
+                <div>
+                  <Title>{state.nameAndAge}</Title>
+                  <Gender>{state.gender}</Gender>
+                  <div className="d-flex">
+                    <span className="me-2"><img style={{}} src={mapPinIcon} alt="Map Icon Here" /></span>
+                    <div className="text-light">{state.distance}</div>
+                  </div>
+                </div>
 
-      <div>About me</div>
-      <div>{state.aboutMe}</div>
+                <div>
+                  <RoundButton>Send message</RoundButton>
+                  <Button className="bg-dark border-0"><img className="rounded-3 ms-2" src={threeDotMenu} alt="Three dot menu" /></Button>
+                </div>
+              </div>
 
-      <div>Appearance</div>
-      <Col>
-        <div>Height</div>
-        <div>{state.height}</div>
-      </Col>
+              {/* Lower */}
+              <div className="d-flex">
+                <img className="rounded-3 d-block m-auto" src={profileImg2} alt="Profile Here" style={{ maxWidth: '92px' }} />
+                <img className="rounded-3 d-block m-auto" src={profileImg3} alt="Profile Here" style={{ maxWidth: '92px' }} />
+                <img className="rounded-3 d-block m-auto" src={profileImg4} alt="Profile Here" style={{ maxWidth: '92px' }} />
+                <img className="rounded-3 d-block m-auto" src={profileImg5} alt="Profile Here" style={{ maxWidth: '92px' }} />
+                <img className="rounded-3 d-block m-auto" src={profileImg6} alt="Profile Here" style={{ maxWidth: '92px' }} />
+                <img className="rounded-3 d-block m-auto" src={profileImg7} alt="Profile Here" style={{ maxWidth: '92px' }} />
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <Col>
-        <div>Body type</div>
-        <div>{state.bodyType}</div>
-      </Col>
+        {/* Mobile and Tablet Section. *LEARN: Hide on xxl and wider screens */}
+        <div className="d-xxl-none">
+          <ProfileMobileView state={state} />
+        </div>
+      </Section>
 
-      <Col>
-        <div>Tattoos</div>
-        <div>{state.tattoos}</div>
-      </Col>
+      <Section>
+        <Heading>About me</Heading>
+        <div className="text-light">{state.aboutMe}</div>
+      </Section>
+
+      <Section>
+        <Heading>Appearance</Heading>
+        <Row className="gy-3">
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Height</SubHeading>
+            <div className="fw-bold">{state.height}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Body type</SubHeading>
+            <div className="fw-bold">{state.bodyType}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Tattoos</SubHeading>
+            <div className="fw-bold">{state.tattoos}</div>
+          </Col>
+        </Row>
+      </Section>
+
+      <Section>
+        <Heading>Basic Info</Heading>
+        <Row className="gy-3">
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>I&apos;m looking for</SubHeading>
+            <div className="fw-bold">{state.lookingFor}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Relationship Status</SubHeading>
+            <div className="fw-bold">{state.relationshipState}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Sexual Orientation</SubHeading>
+            <div className="fw-bold">{state.sexualOrientation}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Parental Status</SubHeading>
+            <div className="fw-bold">{state.parentalStatus}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Employment</SubHeading>
+            <div className="fw-bold">{state.employment}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Education Level</SubHeading>
+            <div className="fw-bold">{state.educationLevel}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Drinking</SubHeading>
+            <div className="fw-bold">{state.drinking}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Smoking</SubHeading>
+            <div className="fw-bold">{state.smoking}</div>
+          </Col>
+          <Col xs="6" md="4" lg="6" xl="4">
+            <SubHeading>Religion</SubHeading>
+            <div className="fw-bold">{state.religion}</div>
+          </Col>
+        </Row>
+      </Section>
+
+      <Section>
+        <Heading>Interests</Heading>
+        <div className="fw-bold text-nowrap d-flex flex-wrap">{state.interests.map((interest) => <span key={interest} className="me-3 rounded-5 px-3 py-1" style={{ background: '#383838', margin: '0.625rem 0' }}>{interest}</span>)}</div>
+      </Section>
+
     </DatingPageWrapper>
   );
 }

9dd1845bdee6959c993e241f0ecbd1110e07971b
Eric O <eric@ejdigitalmedia.com>
2022-12-16 01:49:09 -0500

    Add additional tests

--- test/e2e/feed-comments/find-feed-comments-with-replies.e2e-spec.ts
+++ test/e2e/feed-comments/find-feed-comments-with-replies.e2e-spec.ts
@@ -165,7 +165,7 @@ describe('Find Feed Comments With Replies (e2e)', () => {
     });
 
     describe('when `after` argument is supplied', () => {
-      it('get expected first and second sets of paginated results', async () => {
+      beforeEach(async () => {
         const feedComments1 = await feedCommentsService
           .createFeedComment(
             feedPost.id,
@@ -185,6 +185,20 @@ describe('Find Feed Comments With Replies (e2e)', () => {
             feedPost.id,
             activeUser._id.toString(),
             'Hello Test Message 3',
+            commentImages,
+          );
+        const feedComments4 = await feedCommentsService
+          .createFeedComment(
+            feedPost.id,
+            activeUser._id.toString(),
+            'Hello Test Message 4',
+            commentImages,
+          );
+        const feedComments5 = await feedCommentsService
+          .createFeedComment(
+            feedPost.id,
+            activeUser._id.toString(),
+            'Hello Test Message 5',
             commentImages,
           );
         await feedCommentsService
@@ -215,23 +229,60 @@ describe('Find Feed Comments With Replies (e2e)', () => {
             'Hello Test Reply Message 4',
             commentImages,
           );
-        const limit = 2;
-        const firstResponse = await request(app.getHttpServer())
-          .get(`/feed-comments?feedPostId=${feedPost._id}&limit=${limit}&sortBy=newestFirst`)
-          .auth(activeUserAuthToken, { type: 'bearer' })
-          .send();
-        for (let index = 1; index < firstResponse.body.length; index += 1) {
-          expect(firstResponse.body[index].createdAt < firstResponse.body[index - 1].createdAt).toBe(true);
-        }
-        expect(firstResponse.body).toHaveLength(2);
-        const secondResponse = await request(app.getHttpServer())
-          .get(`/feed-comments?feedPostId=${feedPost._id}&limit=${limit}&sortBy=newestFirst&after=${firstResponse.body[limit - 1]._id}`)
-          .auth(activeUserAuthToken, { type: 'bearer' })
-          .send();
-        for (let index = 1; index < secondResponse.body.length; index += 1) {
-          expect(secondResponse.body[index].createdAt < secondResponse.body[index - 1].createdAt).toBe(true);
-        }
-        expect(secondResponse.body).toHaveLength(1);
+        await feedCommentsService
+          .createFeedReply(
+            feedComments4._id.toString(),
+            activeUser._id.toString(),
+            'Hello Test Reply Message 5',
+            commentImages,
+          );
+        await feedCommentsService
+          .createFeedReply(
+            feedComments5._id.toString(),
+            activeUser._id.toString(),
+            'Hello Test Reply Message 6',
+            commentImages,
+          );
+      });
+      describe('get expected first and second sets of paginated results', () => {
+        it('when sort is newestFirst', async () => {
+          const limit = 3;
+          const firstResponse = await request(app.getHttpServer())
+            .get(`/feed-comments?feedPostId=${feedPost._id}&limit=${limit}&sortBy=newestFirst`)
+            .auth(activeUserAuthToken, { type: 'bearer' })
+            .send();
+          for (let index = 1; index < firstResponse.body.length; index += 1) {
+            expect(firstResponse.body[index].createdAt < firstResponse.body[index - 1].createdAt).toBe(true);
+          }
+          expect(firstResponse.body).toHaveLength(3);
+          const secondResponse = await request(app.getHttpServer())
+            .get(`/feed-comments?feedPostId=${feedPost._id}&limit=${limit}&sortBy=newestFirst&after=${firstResponse.body[limit - 1]._id}`)
+            .auth(activeUserAuthToken, { type: 'bearer' })
+            .send();
+          for (let index = 1; index < secondResponse.body.length; index += 1) {
+            expect(secondResponse.body[index].createdAt < secondResponse.body[index - 1].createdAt).toBe(true);
+          }
+          expect(secondResponse.body).toHaveLength(2);
+        });
+        it('when sort is oldestFirst', async () => {
+          const limit = 3;
+          const firstResponse = await request(app.getHttpServer())
+            .get(`/feed-comments?feedPostId=${feedPost._id}&limit=${limit}&sortBy=oldestFirst`)
+            .auth(activeUserAuthToken, { type: 'bearer' })
+            .send();
+          for (let index = 1; index < firstResponse.body.length; index += 1) {
+            expect(firstResponse.body[index].createdAt > firstResponse.body[index - 1].createdAt).toBe(true);
+          }
+          expect(firstResponse.body).toHaveLength(3);
+          const secondResponse = await request(app.getHttpServer())
+            .get(`/feed-comments?feedPostId=${feedPost._id}&limit=${limit}&sortBy=oldestFirst&after=${firstResponse.body[limit - 1]._id}`)
+            .auth(activeUserAuthToken, { type: 'bearer' })
+            .send();
+          for (let index = 1; index < secondResponse.body.length; index += 1) {
+            expect(secondResponse.body[index].createdAt > secondResponse.body[index - 1].createdAt).toBe(true);
+          }
+          expect(secondResponse.body).toHaveLength(2);
+        });
       });
     });
 

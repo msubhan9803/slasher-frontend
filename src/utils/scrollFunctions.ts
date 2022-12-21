98469c3e66929c86a48fd96e12f16ad4d5162bb9
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-21 13:56:00 +0530

    hotfix for sparingly occuring scrll error reported by Eric

--- src/utils/scrollFunctions.ts
+++ src/utils/scrollFunctions.ts
@@ -1,9 +1,10 @@
 export const scrollWithOffset = (el: any) => {
   // alert('scrollWithOffset?');
   const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-  const yOffset = -80; // for desktop
-  // const yOffset = 0;
-  window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
+  const yOffset = -120; // for desktop
+  setTimeout(() => {
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
+  }, 500);
 };
 
 export const scrollToTop = () => {

6152a5be8cb8581b0bc85965f0c808a30a97ba84
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-21 21:09:01 +0530

    Remove unnecessary comment

--- src/components/ui/RoundButton.tsx
+++ src/components/ui/RoundButton.tsx
@@ -1,8 +1,6 @@
 import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
 
-/* NOTE: Sahil: I am adding inversion of color and background only if we pass `$invertColorOnHover`
-prop becoz in other places this might not be the default desired behaviour (unsure about it). */
 const RoundButton = styled(Button)`
   border-radius: 50rem;
 `;

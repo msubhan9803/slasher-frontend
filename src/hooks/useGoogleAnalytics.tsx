0ae3c8f40eb82ad352f629f76d2f8dcbb73577e8
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-27 00:45:02 +0530

    Update fix for aggrestive event sending by react.

--- src/hooks/useGoogleAnalytics.tsx
+++ src/hooks/useGoogleAnalytics.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import useScript from './useScript';
 
@@ -11,6 +11,7 @@ declare global {
 const useGoogleAnalytics = (analyticsId: string) => {
   const location = useLocation();
   const isLoaded = useScript(`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`);
+  const previousPathRef = useRef<string>();
 
   const { pathname, search, hash } = location;
 
@@ -23,11 +24,17 @@ const useGoogleAnalytics = (analyticsId: string) => {
       // eslint-disable-next-line prefer-rest-params
       window.dataLayer.push(arguments);
     }
+
+    const path = pathname + search + hash;
+    if (previousPathRef.current === path) return;
+    previousPathRef.current = path;
+
     gtag('js', new Date()); // necessary
     gtag('config', analyticsId); // necessary
 
+    // console.log('event sent for path?', path); // USE THIS TO TEST THE ACTUALY SENDING OF EVENT.
     gtag('event', 'page_view', {
-      page_location: pathname + search + hash,
+      page_location: path,
       page_title: document.title,
     });
   }, [location, isLoaded]);

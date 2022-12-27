c70c086acd5adf752f716384f82e4cdeca49ed20
Sahil Rajput <sahilrajput03@gmail.com>
2022-12-27 14:06:27 +0530

    chore: remove alert statement

--- src/utils/scrollFunctions.ts
+++ src/utils/scrollFunctions.ts
@@ -1,7 +1,6 @@
 export const scrollWithOffset = (el: any) => {
-  // alert('scrollWithOffset?');
   const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
-  const yOffset = -120; // for desktop
+  const yOffset = -120;
   setTimeout(() => {
     window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' });
   }, 500);

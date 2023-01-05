af91ef95be5f0f5f760d9043e9b8e54f8d040647
Avadh Ladani <avadh.ladani353@gmail.com>
2023-01-05 16:17:28 +0530

    - fix user friend list issue in sidebar

--- src/components/layout/right-sidebar-wrapper/components/Friends.tsx
+++ src/components/layout/right-sidebar-wrapper/components/Friends.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { getUsersFriends } from '../../../../api/users';
+import { useParams } from 'react-router-dom';
+import { getUser, getUsersFriends } from '../../../../api/users';
+import LoadingIndicator from '../../../ui/LoadingIndicator';
 import FriendCircleWithLabel from './FriendCircleWithLabel';
 import SidebarHeaderWithLink from './SidebarHeaderWithLink';
-import { useAppSelector } from '../../../../redux/hooks';
 
 interface FriendProps {
   /* eslint no-underscore-dangle: 0 */
@@ -14,37 +15,43 @@ interface FriendProps {
 
 function Friends() {
   const [friendsList, setFriendsList] = useState<FriendProps[]>([]);
-  const sidebarContext = useAppSelector((state) => state.sidebarContext);
-  const sidebarUserIdRef = useRef('');
+  const [user, setUserId] = useState<any>([]);
+  const [loader, setLoader] = useState<any>(false);
+  const { userName: userNameOrId } = useParams<string>();
+
+  const getUserFriendsss = (id: string) => {
+    getUsersFriends(id)
+      .then((res) => { setFriendsList(res.data.friends); setLoader(false); });
+  };
 
   useEffect(() => {
-    setFriendsList([]);
-    const isSameUserId = sidebarUserIdRef.current === sidebarContext.userId;
-    if (isSameUserId) return;
-    if (sidebarContext.userId) {
-      sidebarUserIdRef.current = sidebarContext.userId;
-      getUsersFriends(sidebarContext.userId)
-        .then((res) => setFriendsList(res.data.friends));
+    if (userNameOrId) {
+      setLoader(true);
+      getUser(userNameOrId)
+        .then((res) => { getUserFriendsss(res.data.id); setUserId(res.data); });
     }
-  }, [sidebarContext]);
+  }, [userNameOrId]);
 
   return (
     <>
-      <SidebarHeaderWithLink headerLabel="Friends" linkLabel="See All" linkTo={`/${sidebarContext.userName}/friends`} />
+      <SidebarHeaderWithLink headerLabel="Friends" linkLabel="See All" linkTo={`/${user.userName}/friends`} />
       <div className="p-3 bg-dark rounded-3">
         <Row>
-          {friendsList.map((friend: FriendProps, i: number) => (
-            /* eslint no-underscore-dangle: 0 */
-            <Col xs="4" key={friend._id} className={i > 2 ? 'mt-3' : ''}>
-              <FriendCircleWithLabel
-                className="mx-auto"
-                photo={friend.profilePic}
-                label={friend.userName}
-                photoAlt={`${i}`}
-                linkTo={`/${friend.userName}`}
-              />
-            </Col>
-          ))}
+          {
+            loader ? <LoadingIndicator />
+              : friendsList.map((friend: FriendProps, i: number) => (
+                /* eslint no-underscore-dangle: 0 */
+                <Col xs="4" key={friend._id} className={i > 2 ? 'mt-3' : ''}>
+                  <FriendCircleWithLabel
+                    className="mx-auto"
+                    photo={friend.profilePic}
+                    label={friend.userName}
+                    photoAlt={`${i}`}
+                    linkTo={`/${friend.userName}`}
+                  />
+                </Col>
+              ))
+}
         </Row>
       </div>
     </>

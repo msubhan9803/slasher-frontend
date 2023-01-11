15976d42073534927386dd3ffae23c816ad27709
Sahil Rajput <sahilrajput03@gmail.com>
2023-01-12 02:08:15 +0530

    Improve e2e tets with toEqual assertions.

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller, Get, HttpException, HttpStatus, Patch, Req,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { pick } from '../utils/object-utils';
 import { getUserFromRequest } from '../utils/request-utils';
 import { UpdateNoticationSettingDto } from './dto/update-notification-setting.dto';
 import { UserSettingsService } from './providers/user-settings.service';
@@ -20,7 +21,16 @@ export class SettingsController {
     if (!userSetting) {
       throw new HttpException('User setting not found', HttpStatus.NOT_FOUND);
     }
-    return userSetting;
+    return pick(userSetting, ['_id', 'app_tutorial', 'dating_got_a_match',
+      'dating_message_received', 'event_reminder', 'event_suggested',
+      'feed_comment_on_post', 'feed_mention_on_post_comment_reply',
+      'feed_post_like', 'friends_got_a_match', 'friends_message_received',
+      'message_board_like_your_post', 'message_board_mention_on_comment_reply',
+      'message_board_new_post_on_thread', 'message_board_reply_your_post',
+      'movie_comment_on_reply', 'movie_comment_reply_like',
+      'movie_mention_on_comment_reply', 'onboarding_completed',
+      'rss_feed_mention_on_post_comment_reply', 'rss_feed_post_like', 'userId',
+    ]);
   }
 
   @Patch('notifications')
@@ -30,6 +40,15 @@ export class SettingsController {
   ) {
     const user = getUserFromRequest(request);
     const userSetting = await this.userSettingsService.update(user._id, updateNoticationSettingDto);
-    return userSetting;
+    return pick(userSetting, ['_id', 'app_tutorial', 'dating_got_a_match',
+      'dating_message_received', 'event_reminder', 'event_suggested',
+      'feed_comment_on_post', 'feed_mention_on_post_comment_reply',
+      'feed_post_like', 'friends_got_a_match', 'friends_message_received',
+      'message_board_like_your_post', 'message_board_mention_on_comment_reply',
+      'message_board_new_post_on_thread', 'message_board_reply_your_post',
+      'movie_comment_on_reply', 'movie_comment_reply_like',
+      'movie_mention_on_comment_reply', 'onboarding_completed',
+      'rss_feed_mention_on_post_comment_reply', 'rss_feed_post_like', 'userId',
+    ]);
   }
 }

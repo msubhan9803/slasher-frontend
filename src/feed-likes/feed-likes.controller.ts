9784a791b3bb40eb78a65a1ed9df8c00cc679aa1
Avadh Ladani <avadh.ladani353@gmail.com>
2023-01-16 16:06:25 +0530

    changes in createfeedpostlike function

--- src/feed-likes/feed-likes.controller.ts
+++ src/feed-likes/feed-likes.controller.ts
@@ -32,13 +32,15 @@ export class FeedLikesController {
     }
     await this.feedLikesService.createFeedPostLike(params.feedPostId, user._id);
 
-    await this.notificationsService.create({
-      userId: post.userId as any,
-      feedPostId: { _id: post._id } as unknown as FeedPost,
-      senderId: user._id,
-      notifyType: NotificationType.UserLikedYourPost,
-      notificationMsg: 'liked your post',
-    });
+    if (!post.rssfeedProviderId) {
+      await this.notificationsService.create({
+        userId: post.userId as any,
+        feedPostId: { _id: post._id } as unknown as FeedPost,
+        senderId: user._id,
+        notifyType: NotificationType.UserLikedYourPost,
+        notificationMsg: 'liked your post',
+      });
+    }
     return { success: true };
   }
 

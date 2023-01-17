700bd2013ee6811f052ac311841970580d1c9226
Avadh Ladani <avadh.ladani353@gmail.com>
2023-01-17 19:56:31 +0530

    fix lint error

--- src/routes/profile/ProfileFriends/ProfileFriends.tsx
+++ src/routes/profile/ProfileFriends/ProfileFriends.tsx
@@ -165,9 +165,9 @@ function ProfileFriends({ user }: Props) {
       .then((res) => {
         setShow(false);
         if (res.status === 201) {
-          const updateFriendsList = friendsList.filter((friend: any) => {
-            return friend._id !== popoverClick?.id;
-          });
+          const updateFriendsList = friendsList.filter(
+            (friend: any) => friend._id !== popoverClick?.id,
+          );
           setFriendsList(updateFriendsList);
           setFriendCount(friendCount ? friendCount - 1 : 0);
         }

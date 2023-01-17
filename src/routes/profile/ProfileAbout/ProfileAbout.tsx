4afd0ddcb25a3d564fc6c76fc45b4237adf9fb58
Avadh Ladani <avadh.ladani353@gmail.com>
2023-01-17 12:55:16 +0530

    integrate update profile about me api

--- src/routes/profile/ProfileAbout/ProfileAbout.tsx
+++ src/routes/profile/ProfileAbout/ProfileAbout.tsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Col, Form, Row } from 'react-bootstrap';
+import {
+  Button, Col, Form, Row,
+} from 'react-bootstrap';
 import styled from 'styled-components';
 import AuthenticatedPageWrapper from '../../../components/layout/main-site-wrapper/authenticated/AuthenticatedPageWrapper';
 import ProfileHeader from '../ProfileHeader';
 import RoundButton from '../../../components/ui/RoundButton';
 import { User } from '../../../types';
 import { useAppSelector } from '../../../redux/hooks';
+import { updateUserAbout } from '../../../api/users';
 
 const CustomSpan = styled(Form.Text)`
   margin-top: -1.43rem;
   margin-right: .5rem;
 `;
-const AboutMessage = 'Hi, i am Aly, i am 26 years old and  worked as a UI/UX design in  Slasher Corp. In my spare time, I enjoy going to the gym and regularly partake in charity runs around the UK in order to help the community and to stay fit and healthy.  Skills: NodeJS, Python, Interface, GTK Lipsum Rails, .NET Groovy Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ';
+const CustomDiv = styled.div`
+  white-space: pre;
+`;
 
 interface Props {
   user: User
 }
 function ProfileAbout({ user }: Props) {
   const [isEdit, setEdit] = useState<boolean>(false);
-  const [message, setMessage] = useState<string>(AboutMessage);
+  const [message, setMessage] = useState<string>(user?.aboutMe || '');
   const [charCount, setCharCount] = useState<number>(0);
   const loginUserId = useAppSelector((state) => state.user.user.id);
 
@@ -29,6 +34,12 @@ function ProfileAbout({ user }: Props) {
     setMessage(e.target.value);
   };
 
+  const handleUserAbout = (id: string) => {
+    updateUserAbout(id, message).then((res) => {
+      setMessage(res.data.aboutMe);
+      setEdit(!isEdit);
+    });
+  };
   return (
     <AuthenticatedPageWrapper rightSidebarType={loginUserId === user?.id ? 'profile-self' : 'profile-other-user'}>
       <ProfileHeader tabKey="about" user={user} />
@@ -36,7 +47,11 @@ function ProfileAbout({ user }: Props) {
         <div className="d-flex justify-content-between">
           <h2 className="mb-4">About me</h2>
           {loginUserId === user?.id
-            && <FontAwesomeIcon icon={solid('pen')} className="me-1 mt-1" size="lg" onClick={() => setEdit(!isEdit)} />}
+            && (
+              <Button variant="link" onClick={() => setEdit(!isEdit)}>
+                <FontAwesomeIcon icon={solid('pen')} className="me-1 mt-1" size="lg" />
+              </Button>
+            )}
         </div>
         {isEdit
           ? (
@@ -65,7 +80,7 @@ function ProfileAbout({ user }: Props) {
                       </RoundButton>
                     </Col>
                     <Col xs={6}>
-                      <RoundButton className="w-100" variant="primary" onClick={() => setEdit(!isEdit)}>
+                      <RoundButton className="w-100" variant="primary" onClick={() => handleUserAbout(user?.id)}>
                         Save
                       </RoundButton>
                     </Col>
@@ -75,9 +90,9 @@ function ProfileAbout({ user }: Props) {
             </div>
           )
           : (
-            <div>
-              {user.aboutMe}
-            </div>
+            <CustomDiv>
+              {message}
+            </CustomDiv>
           )}
       </div>
     </AuthenticatedPageWrapper>

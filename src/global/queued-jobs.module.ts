6cb966ec40f59726b496bb0da5d014e64f31bb64
Avadh Ladani <avadh.ladani353@gmail.com>
2023-01-20 12:37:13 +0530

    changes in queuejobs module

--- src/global/queued-jobs.module.ts
+++ src/global/queued-jobs.module.ts
@@ -1,5 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
+import { ConfigService } from '@nestjs/config';
 import { MessageCountUpdateConsumer } from '../jobs/consumers/message-count-update.consumer';
 import { ChatModule } from '../chat/chat.module';
 import { ChatGateway } from '../chat/providers/chat.gateway';
@@ -7,16 +8,19 @@ import { ChatGateway } from '../chat/providers/chat.gateway';
 @Global()
 @Module({
   imports: [
-    BullModule.forRoot({
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
         redis: {
-          host: 'localhost',
-          port: 6379,
+          host: config.get<string>('REDIS_HOST'),
+          port: config.get<number>('REDIS_PORT'),
         },
       }),
-      BullModule.registerQueue({
-        name: 'message-count-update',
-      }),
-      ChatModule,
+    }),
+    BullModule.registerQueue({
+      name: 'message-count-update',
+    }),
+    ChatModule,
   ],
   providers: [MessageCountUpdateConsumer, ChatGateway],
   exports: [BullModule],

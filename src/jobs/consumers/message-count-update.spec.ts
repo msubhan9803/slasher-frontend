15ce34e1e849dd7c7394e332a83e89ee683ffda5
Sahil Rajput <sahilrajput03@gmail.com>
2023-02-01 02:11:29 +0530

    Update consumer test.

--- src/jobs/consumers/message-count-update.spec.ts
+++ src/jobs/consumers/message-count-update.spec.ts
@@ -1,10 +1,7 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { getQueueToken } from '@nestjs/bull';
 import { INestApplication } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { getConnectionToken } from '@nestjs/mongoose';
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { Connection } from 'mongoose';
 import { userFactory } from '../../../test/factories/user.factory';
 import { clearDatabase } from '../../../test/helpers/mongo-helpers';
@@ -13,18 +10,18 @@ import { ChatGateway } from '../../chat/providers/chat.gateway';
 import { ChatService } from '../../chat/providers/chat.service';
 import { UserDocument } from '../../schemas/user/user.schema';
 import { UsersService } from '../../users/providers/users.service';
+import { MessageCountUpdateConsumer } from './message-count-update.consumer';
 
-describe('EnqueuerService', () => {
+describe('#message-count-update', () => {
   let app: INestApplication;
-  let module: TestingModule;
   let mockQueue;
   let connection: Connection;
   let chatService: ChatService;
   let chatGateway: ChatGateway;
   let activeUser: UserDocument;
-  let activeUserAuthToken: string;
   let usersService: UsersService;
-  let configService: ConfigService;
+  let messageCountUpdateConsumer: MessageCountUpdateConsumer;
+  let user1: UserDocument;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -39,34 +36,28 @@ describe('EnqueuerService', () => {
       usersService = moduleRef.get<UsersService>(UsersService);
       chatService = moduleRef.get<ChatService>(ChatService);
       chatGateway = moduleRef.get<ChatGateway>(ChatGateway);
-      configService = moduleRef.get<ConfigService>(ConfigService);
-      messageCountUpdateQueue = moduleRef.get(ConfigService);
+      messageCountUpdateConsumer = moduleRef.get(MessageCountUpdateConsumer);
 
       app = moduleRef.createNestApplication();
   });
 
-  afterAll(async () => module.close());
+  afterAll(async () => app.close());
 
   beforeEach(async () => {
     // Drop database so we start fresh before each test
     await clearDatabase(connection);
 
     activeUser = await usersService.create(userFactory.build());
-    activeUserAuthToken = activeUser.generateNewJwtToken(
-      configService.get<string>('JWT_SECRET_KEY'),
-    );
+    user1 = await usersService.create(userFactory.build({ userName: 'Hannibal' }));
   });
 
-  describe('#foo', () => {
+  describe('should call `#chatGateway.emitMessageCountUpdateEvent` with appropriate userId', () => {
     it('adds a job', async () => {
-      await messageCountUpdateQueue.add(
-        'send-update-if-message-unread',
-        { messageId: 'my_message_id_here' },
-        { delay: 15_000 }, // 15 second delay
-      );
-
+      const message = await chatService.sendPrivateDirectMessage(activeUser.id, user1.id, 'Hi, test message 1.');
       jest.spyOn(chatGateway, 'emitMessageCountUpdateEvent').mockImplementation(() => Promise.resolve(undefined));
-      expect(chatGateway.emitMessageCountUpdateEvent).toHaveBeenCalledWith('my_message_id_here');
+      await messageCountUpdateConsumer.sendUpdateIfMessageUnread({ data: { messageId: message._id.toString() } } as any);
+
+      expect(chatGateway.emitMessageCountUpdateEvent).toHaveBeenCalledWith(message.senderId.toString());
     });
   });
 });

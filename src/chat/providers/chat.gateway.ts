e9e539ca664511d19f26e1a9af8b0e380a5a9e81
Eric O <eric@ejdigitalmedia.com>
2023-02-01 01:50:34 -0500

    Return additional fields in ChatGateway#chatMessage

--- src/chat/providers/chat.gateway.ts
+++ src/chat/providers/chat.gateway.ts
@@ -58,7 +58,10 @@ export class ChatGateway {
     const messageObject = await this.chatService.sendPrivateDirectMessage(fromUserId, toUserId, data.message);
     const targetUserSocketIds = await this.usersService.findSocketIdsForUser(toUserId);
     targetUserSocketIds.forEach((socketId) => {
-      client.to(socketId).emit('chatMessageReceived', { message: pick(messageObject, ['_id', 'message', 'matchId', 'createdAt']), user });
+      client.to(socketId).emit('chatMessageReceived', {
+        message: pick(messageObject, ['_id', 'image', 'message', 'fromId', 'senderId', 'matchId', 'createdAt']),
+        user,
+      });
     });
     await this.messageCountUpdateQueue.add(
       'send-update-if-message-unread',

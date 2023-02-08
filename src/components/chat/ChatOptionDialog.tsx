9d6ad01dde7e8078d9e21102e75fcf151cb61a49
Sahil Rajput <sahilrajput03@gmail.com>
2023-02-09 02:46:22 +0530

    Fix textual typo

--- src/components/chat/ChatOptionDialog.tsx
+++ src/components/chat/ChatOptionDialog.tsx
@@ -55,7 +55,7 @@ function ChatOptionDialog({
       {slectedDropdownValue === 'Delete' && (
         <Modal.Body className="d-flex flex-column align-items-center text-center pt-0">
           <h1 className="h3 mb-0 text-primary">Delete</h1>
-          <p className="px-3">Are you sure you want to delete this conversation?1</p>
+          <p className="px-3">Are you sure you want to delete this conversation?</p>
           <RoundButton className="mb-3 w-100" onClick={handleDeleteConversationMessages}>Yes</RoundButton>
           <RoundButton className="mb-3 w-100 bg-dark border-dark shadow-none text-white" onClick={closeModal}>Cancel</RoundButton>
         </Modal.Body>

22ed3ee9f8b2b303e410674a2369b62b4bdc2c2d
Eric O <eric@ejdigitalmedia.com>
2023-02-27 22:24:14 -0500

    Add remote-constants endpoint; Also create default 'No content' fallback page ServeStaticModule

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,8 +1,12 @@
 /* eslint-disable class-methods-use-this */
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { relativeToFullImagePath } from '../utils/image-utils';
 
 @Controller()
 export class AppController {
+  constructor(private configService: ConfigService) { }
+
   // This route handler only exists to provide a nice-looking response at the top level "/" url
   @Get()
   index(): object {
@@ -19,6 +23,14 @@ export class AppController {
   @Get('api/v1')
   apiV1() {
     return this.getVersionResponse();
+  }
+
+  // Returns various server-side values that should be treated as constants on the client side
+  @Get('api/v1/remote-constants')
+  remoteConstants() {
+    return {
+      placeholderUrlNoImageAvailable: relativeToFullImagePath(this.configService, '/placeholders/no_image_available.png'),
+    };
   }
 
   // This route handler only exists to provide a 200 status and "ok" message as a health check endpoint

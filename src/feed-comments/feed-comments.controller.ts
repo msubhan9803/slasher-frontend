88742ca64564b8645614dc063a024d405e62a537
Avadh Ladani <avadh.ladani353@gmail.com>
2023-04-04 18:11:27 +0530

    update lastupdatedat on feedpost with test cases

--- src/feed-comments/feed-comments.controller.ts
+++ src/feed-comments/feed-comments.controller.ts
@@ -193,13 +193,13 @@ export class FeedCommentsController {
         },
       );
     }
-
     const updatedComment = await this.feedCommentsService.updateFeedComment(params.feedCommentId, updateFeedCommentsDto);
+    await this.feedPostsService.updateLastUpdateAt(comment.feedPostId.toString());
+
     const mentionedUserIdsBeforeUpdate = extractUserMentionIdsFromMessage(comment.message);
     const mentionedUserIdsAfterUpdate = extractUserMentionIdsFromMessage(updatedComment?.message);
 
     await this.sendFeedCommentUpdateNotifications(user, updatedComment, mentionedUserIdsBeforeUpdate, mentionedUserIdsAfterUpdate);
-
     return pick(updatedComment, ['_id', 'feedPostId', 'message', 'userId', 'images']);
   }
 
@@ -371,11 +371,12 @@ export class FeedCommentsController {
     }
 
     const updatedReply = await this.feedCommentsService.updateFeedReply(params.feedReplyId, updateFeedReplyDto);
+    await this.feedPostsService.updateLastUpdateAt(reply.feedPostId.toString());
+
     const mentionedUserIdsBeforeUpdate = extractUserMentionIdsFromMessage(reply.message);
     const mentionedUserIdsAfterUpdate = extractUserMentionIdsFromMessage(updatedReply?.message);
 
     await this.sendFeedReplyUpdateNotifications(user, updatedReply, mentionedUserIdsBeforeUpdate, mentionedUserIdsAfterUpdate);
-
     return pick(updatedReply, ['_id', 'feedPostId', 'message', 'images', 'feedCommentId', 'userId']);
   }
 

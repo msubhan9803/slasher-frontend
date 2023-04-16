8599e118f638a3a42b3a083e5a6494344e39cefd
Eric O <eric@ejdigitalmedia.com>
2023-04-16 19:55:18 -0400

    Increase getMovies() limit param to 40

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -12,7 +12,7 @@ export async function getMovies(
   const headers = {
     Authorization: `Bearer ${token}`,
   };
-  let queryParameter = `?limit=20&sortBy=${sortValue}`;
+  let queryParameter = `?limit=40&sortBy=${sortValue}`;
   if (lastRetrievedMovieId) {
     queryParameter += `&after=${lastRetrievedMovieId}`;
   }

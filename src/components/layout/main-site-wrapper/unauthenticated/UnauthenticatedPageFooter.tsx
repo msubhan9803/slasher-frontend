6134c9795f11d56dd662ad2461baf356cfd6374e
Eric O <eric@ejdigitalmedia.com>
2023-04-15 23:33:23 -0400

    Update unauthenticated footer links

--- src/components/layout/main-site-wrapper/unauthenticated/UnauthenticatedPageFooter.tsx
+++ src/components/layout/main-site-wrapper/unauthenticated/UnauthenticatedPageFooter.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { APP_STORE_DOWNLOAD_URL, GOOGLE_PLAY_DOWNLOAD_URL } from '../../../../constants';
 
 const StyledFooter = styled.footer`
   .footer-container {
@@ -19,14 +20,14 @@ function UnauthenticatedPageFooter() {
     <StyledFooter>
       <Container fluid="lg" className="footer-container py-4">
         <div className="w-100 d-flex flex-wrap justify-content-evenly">
-          <Link className="px-2" to="/">About</Link>
-          <Link className="px-2" to="/">Help</Link>
-          <Link className="px-2" to="/">Terms of Service</Link>
-          <Link className="px-2" to="/">Privacy Policy</Link>
-          <Link className="px-2" to="/">Cookie Policy</Link>
-          <Link className="px-2" to="/">Site Rules</Link>
-          <Link className="px-2" to="/">Android App</Link>
-          <Link className="px-2" to="/">iOS App</Link>
+          <a className="px-2" href="https://pages.slasher.tv/about">About</a>
+          <a className="px-2" href="https://pages.slasher.tv/help">Help</a>
+          <a className="px-2" href="https://pages.slasher.tv/terms">Terms of Service</a>
+          <a className="px-2" href="https://pages.slasher.tv/privacy-policy">Privacy Policy</a>
+          <a className="px-2" href="https://pages.slasher.tv/cookie-policy">Cookie Policy</a>
+          <a className="px-2" href="https://pages.slasher.tv/rules">Site Rules</a>
+          <a className="px-2" href={GOOGLE_PLAY_DOWNLOAD_URL}>Android App</a>
+          <a className="px-2" href={APP_STORE_DOWNLOAD_URL}>iOS App</a>
           <div className="px-2">
             &copy;
             {' '}

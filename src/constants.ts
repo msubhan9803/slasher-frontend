644f7802199ebd61dc6b3dcac57a4ca9399713dc
Eric O <eric@ejdigitalmedia.com>
2023-04-20 13:07:13 -0400

    Add break point note

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,8 @@ export const analyticsId = process.env.REACT_APP_GOOGLE_ANALYTICS_PROPERTY_ID;
 export const MAIN_CONTENT_ID = 'main-content';
 
 export const BREAK_POINTS = {
+  // NOTE: Break points below must be in ascending numeric order.
+  // Other logic in the app relies on this order.
   xs: 0, sm: 576, md: 768, lg: 980, xl: 1200, xxl: 1440,
 };
 

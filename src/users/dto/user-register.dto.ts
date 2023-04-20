2c57e315d9327d15bd247e8542a69a397a6dfdb3
ladaniavadh <avadh.ladani353@gmail.com>
2023-04-20 14:13:00 +0530

    add firstname validator

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -9,10 +9,11 @@ import { IsValidUsername } from '../../app/decorators/class-validator/user-name.
 import { IsValidPassword } from '../../app/decorators/class-validator/valid-password.decorator';
 import { Match } from '../../app/decorators/class-validator/match.decorator';
 import { MinYearsBeforeToday } from '../../app/decorators/class-validator/min-years-before-today.decorator';
+import { IsValidFirstname } from '../../app/decorators/class-validator/first-name.decorator';
 
 export class UserRegisterDto {
   @IsNotEmpty()
-  @MaxLength(30)
+  @IsValidFirstname()
   firstName: string;
 
   @IsNotEmpty()

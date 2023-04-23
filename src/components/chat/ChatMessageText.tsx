402bbe04e8a53d4cf7f5f272a108e522e32798c2
Eric O <eric@ejdigitalmedia.com>
2023-04-23 12:47:04 -0400

    Add ChatMessage component for formatting chat messages (including url-decoding, to handle old API message format)

---
+++ src/components/chat/ChatMessageText.tsx
@@ -0,0 +1,38 @@
+import linkifyHtml from 'linkify-html';
+import React from 'react';
+import { customlinkifyOpts } from '../../utils/linkify-utils';
+import { newLineToBr, decryptMessage, escapeHtmlSpecialCharacters } from '../../utils/text-utils';
+
+export interface Props {
+  message: string;
+  firstLineOnly?: boolean;
+}
+
+function ChatMessageText({ message, firstLineOnly }: Props) {
+  let reformattedMessage = message;
+  if (firstLineOnly) {
+    const indexOfFirstNewLineCharacter = reformattedMessage.indexOf('\n');
+    if (indexOfFirstNewLineCharacter > -1) {
+      reformattedMessage = message.substring(0, indexOfFirstNewLineCharacter);
+    }
+  }
+  reformattedMessage = newLineToBr(
+    // TODO: Remove use of decodeURIComponent once the old API is retired and all old messages
+    // have been updated so that they're not being URI-encoded anymore. The URI-encoding is
+    // either coming from the old API or more likely the iOS and Android apps.  It's not clear
+    // why this is being done, sine non-URI-encoded messages seem to display without a problem.
+    linkifyHtml(decryptMessage(
+      escapeHtmlSpecialCharacters(decodeURIComponent(reformattedMessage)),
+    ), customlinkifyOpts),
+  );
+  return (
+    // eslint-disable-next-line react/no-danger
+    <span dangerouslySetInnerHTML={{ __html: reformattedMessage }} />
+  );
+}
+
+ChatMessageText.defaultProps = {
+  firstLineOnly: false,
+};
+
+export default ChatMessageText;

d135a06eb2596e3240ff61fc28f47bc51cbfe0a2
Avadh Ladani <avadh.ladani353@gmail.com>
2023-04-25 18:32:01 +0530

    - fix rating key props error - fix tab review/id link error - fix ratings undefined errors

--- src/routes/movies/movie-details/AboutMovie.tsx
+++ src/routes/movies/movie-details/AboutMovie.tsx
@@ -85,7 +85,6 @@ const filterEnableDevFeatures = (t: OptionType) => (enableDevFeatures ? true : (
 function AboutMovie({ aboutMovieData, movieData, setMovieData }: AboutMovieData) {
   const [searchParams] = useSearchParams();
   const [movieIdList, setMovieIdList] = useState();
-  const [isReviewDetail, setReviewDetail] = useState(false);
   const selfView = searchParams.get('view') === 'self';
   const tabs = (selfView ? tabsForSelf : tabsForViewer).filter(filterEnableDevFeatures);
   const navigate = useNavigate();
@@ -95,15 +94,6 @@ function AboutMovie({ aboutMovieData, movieData, setMovieData }: AboutMovieData)
   useEffect(() => {
     if (params['*'] === 'edit' && !selfView) { navigate(`/app/movies/${params.id}/details`); }
   });
-
-  // TODO: Can this be removed, now that the comment button no longer links to the comment input?
-  useEffect(() => {
-    if (params && params['*']!.startsWith('reviews/')) {
-      setReviewDetail(true);
-    } else {
-      setReviewDetail(false);
-    }
-  }, [params]);
 
   const [bgColor, setBgColor] = useState<boolean>(false);
   const [movieIconListData, setMovieIconListData] = useState(MovieIconList);
@@ -263,7 +253,14 @@ function AboutMovie({ aboutMovieData, movieData, setMovieData }: AboutMovieData)
 
         <Row className="justify-content-center">
           <Col xs={12}>
-            <TabLinks tabsClass="start" tabsClassSmall="start" tabLink={tabs} toLink={`/app/movies/${params.id}`} selectedTab={isReviewDetail ? 'reviews' : params['*']} params={selfView ? '?view=self' : ''} />
+            <TabLinks
+              tabsClass="start"
+              tabsClassSmall="start"
+              tabLink={tabs}
+              toLink={`/app/movies/${params.id}`}
+              selectedTab={params && params['*']!.startsWith('reviews/') ? 'reviews' : params['*']}
+              params={selfView ? '?view=self' : ''}
+            />
           </Col>
         </Row>
       </div>

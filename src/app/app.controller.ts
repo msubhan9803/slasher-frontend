2e1d66510a1c6ce047ed834b8583e0578892b47a
Eric O <eric@ejdigitalmedia.com>
2023-04-29 00:24:57 -0400

    Add additional temporary logging to AppController#cpuTest for AWS 503 error troubleshooting in staging environment

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable class-methods-use-this */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { relativeToFullImagePath } from '../utils/image-utils';
 import { IpOrForwardedIp } from './decorators/ip-or-forwarded-ip.decorator';
@@ -7,6 +7,8 @@ import { sleep } from '../utils/timer-utils';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
+
   constructor(private configService: ConfigService) { }
 
   // This route handler only exists to provide a nice-looking response at the top level "/" url
@@ -51,7 +53,10 @@ export class AppController {
 
   @Get('api/v1/cpu-test')
   async cpuTest() {
+    const randomInt = Math.floor(Math.random() * 1_000_000);
+    this.logger.debug(`START /api/v1/cpu-test begin (random value: ${randomInt})`);
     this.fibonacci(41); // Temporarily do an expensive computation (for testing)
+    this.logger.debug(`END /api/v1/cpu-test begin (random value: ${randomInt})`);
     return this.getVersionResponse();
   }
 

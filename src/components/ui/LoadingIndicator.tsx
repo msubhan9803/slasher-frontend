10dfd7b708b3296d83197dbdb20be79df71d77e3
Sahil Rajput <sahilrajput03@gmail.com>
2023-05-06 23:28:00 +0530

    Fix spinner to be in middle vertically on mobile and desktop.

--- src/components/ui/LoadingIndicator.tsx
+++ src/components/ui/LoadingIndicator.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 
-type Props = { className?: string };
-function LoadingIndicator({ className }: Props) {
+type Props = { className?: string, style?: React.CSSProperties };
+function LoadingIndicator({ className, style }: Props) {
   return (
-    <div className={`text-center ${className}`}>
+    <div className={`text-center ${className}`} style={style}>
       <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
@@ -14,6 +14,7 @@ function LoadingIndicator({ className }: Props) {
 
 LoadingIndicator.defaultProps = {
   className: '',
+  style: {},
 };
 
 export default LoadingIndicator;

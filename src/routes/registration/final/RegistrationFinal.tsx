519b7d15a9862ab1dd2a5a490f2b5eedbdd81994
Avadh Ladani <avadh.ladani353@gmail.com>
2023-05-10 12:01:28 +0530

    SD-1180: final registration page change

--- src/routes/registration/final/RegistrationFinal.tsx
+++ src/routes/registration/final/RegistrationFinal.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { regular } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAppSelector } from '../../../redux/hooks';
+import RoundButton from '../../../components/ui/RoundButton';
 
 function RegistrationFinal() {
   const emailAddress = useAppSelector((state) => state.registration.email);
   return (
     <div className="text-center">
       <FontAwesomeIcon icon={regular('paper-plane')} size="6x" className="text-primary" />
-      <h1 className="h2 mt-4">One more step!</h1>
-      <h2 className="h3 mb-4">Verify your email</h2>
-      <p>We have sent an email to</p>
+      <h1 className="h2 mt-4">You’re almost there!</h1>
+      <h2 className="h3 mb-4">We sent you an email to activate your Slasher account. Once you activate your account, you will be able to sign in.</h2>
+      <p>The email was sent to</p>
       <p className="fs-3 text-primary fw-bold my-3">{emailAddress}</p>
       <p>
-        Click or tap the button in the email to activate your
-        Slasher account.
+        Don’t see the account activation email? Be sure to check your spam folder.
       </p>
       <p>
-        If you do not receive the account activation email, be sure to check your junk or spam
-        folder.
+        If you don’t receive the email within 30 minutes,
+        {' '}
+        <Link to="/app/verification-email-not-received" className="text-primary">click here.</Link>
       </p>
       <p>
-        We look forward to seeing you and hope you have a great time with us!
+        We look forward to seeing you on Slasher!
       </p>
+      <RoundButton id="sign-in-button" type="submit" className="w-25 my-3" variant="primary">
+        Sign in
+      </RoundButton>
       <div className="d-flex flex-md-row justify-content-center flex-column mt-5">
         <p className="fs-4">Need help?</p>
-        <p className="fs-4">&nbsp;Please email help@slasher.tv for assistance.</p>
+        <p className="fs-4">
+          &nbsp;Email
+          <a href="mailto:help@slasher.tv" className="text-decoration-none text-primary">
+            help@slasher.tv
+          </a>
+        </p>
       </div>
     </div>
   );

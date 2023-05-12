8be79dee66bb750c88c40fbb3fe39835e71dd87f
Eric O <eric@ejdigitalmedia.com>
2023-05-11 22:53:07 -0400

    Change registration min age to 18 (per request from Damon); Update new-account-verification email content

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -40,7 +40,7 @@ export class UserRegisterDto {
   securityAnswer: string;
 
   @IsNotEmpty()
-  @MinYearsBeforeToday(17, { message: 'You must be at least 17 to register' })
+  @MinYearsBeforeToday(18, { message: 'You must be at least 18 to register' })
   @Matches(/^\d{4}(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$/i, {
     message: 'Invalid date of birth',
   })

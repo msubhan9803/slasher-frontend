e7b34b8e874a9614eaaaeb469108fbcffecfd64d
Avadh Ladani <avadh.ladani353@gmail.com>
2023-05-19 09:12:35 +0530

    remove unused line of code from event header

--- src/routes/events/EventHeader.tsx
+++ src/routes/events/EventHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import RoundButton from '../../components/ui/RoundButton';
 import TabLinks from '../../components/ui/Tabs/TabLinks';
 import { enableDevFeatures } from '../../utils/configEnvironment';
@@ -15,13 +15,12 @@ const tabs = [
 ];
 const allTabs = enableDevFeatures ? tabs : tabs.filter((t) => !t.devOnly);
 function EventHeader({ tabKey }: EventHeaderProps) {
-  const location = useLocation();
   return (
     <>
       <TabLinks tabLink={allTabs} toLink="/app/events" selectedTab={tabKey} />
       <Row className="justify-content-center mt-4 d-lg-none">
         <Col md={6}>
-          <Link to="/app/events/suggestion" state={{ prev: location.pathname }}>
+          <Link to="/app/events/suggestion">
             <RoundButton className="w-100 fs-4">Suggest event</RoundButton>
           </Link>
         </Col>

b90262875612ade25eed7804b10b6e83b55dd625
Sahil Rajput <sahilrajput03@gmail.com>
2023-05-27 23:00:47 +0530

    SD-1156: Use ternary operator instead of if conditions in pageStateCache.ts file.

--- src/pageStateCache.ts
+++ src/pageStateCache.ts
@@ -5,26 +5,14 @@ const pageStateCache = new Map<string, any>();
 export const getPageName = (location: Location) => location.pathname + location.search + location.hash;
 
 export const hasPageStateCache = (location: Location) => pageStateCache.has(getPageName(location));
-export const deletePageStateCache = (location: Location | string) => {
-  if (typeof location === 'string') {
-    pageStateCache.delete(location);
-    return;
-  }
-  pageStateCache.delete(getPageName(location));
-};
-export const getPageStateCache = <T = any>(location: Location | string): T => {
-  if (typeof location === 'string') {
-    return pageStateCache.get(location);
-  }
-  return pageStateCache.get(getPageName(location));
-};
-export const setPageStateCache = <T>(location: Location | string, data: T) => {
-  if (typeof location === 'string') {
-    pageStateCache.set(location, data);
-    return;
-  }
-  pageStateCache.set(getPageName(location), data);
-};
+export const deletePageStateCache = (location: Location | string) => ((typeof location === 'string') ? pageStateCache.delete(location)
+  : pageStateCache.delete(getPageName(location)));
+export const getPageStateCache = <T = any>(location: Location | string): T => (typeof location === 'string'
+  ? pageStateCache.get(location)
+  : pageStateCache.get(getPageName(location)));
+export const setPageStateCache = <T>(location: Location | string, data: T) => (typeof location === 'string'
+  ? pageStateCache.set(location, data)
+  : pageStateCache.set(getPageName(location), data));
 
 // Note to Sahil: Only for debugging (please remove before making PR)
 Object.assign(window, { pageStateCache });

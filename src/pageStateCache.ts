f9491f6884c0c6a7059cce7f6d0fa32d95841a4f
Sahil Rajput <sahilrajput03@gmail.com>
2023-05-27 23:11:17 +0530

    SD-1156: General code refactor in pageStateCache.ts file.

--- src/pageStateCache.ts
+++ src/pageStateCache.ts
@@ -3,16 +3,19 @@ import { Location } from 'react-router-dom';
 export const getPageName = (location: Location) => `${location.pathname}${location.search}${location.hash}`;
 
 const pageStateCache = new Map<string, any>();
-
-export const hasPageStateCache = (location: Location) => pageStateCache.has(getPageName(location));
-export const deletePageStateCache = (location: Location | string) => ((typeof location === 'string') ? pageStateCache.delete(location)
-  : pageStateCache.delete(getPageName(location)));
+// Helper function to manage `pageStateCache`:
+export const hasPageStateCache = (location: Location | string) => (typeof location === 'string'
+  ? pageStateCache.has(location)
+  : pageStateCache.has(getPageName(location)));
 export const getPageStateCache = <T = any>(location: Location | string): T => (typeof location === 'string'
   ? pageStateCache.get(location)
   : pageStateCache.get(getPageName(location)));
 export const setPageStateCache = <T>(location: Location | string, data: T) => (typeof location === 'string'
   ? pageStateCache.set(location, data)
   : pageStateCache.set(getPageName(location), data));
+export const deletePageStateCache = (location: Location | string) => (typeof location === 'string'
+  ? pageStateCache.delete(location)
+  : pageStateCache.delete(getPageName(location)));
 
 // Note to Sahil: Only for debugging (please remove before making PR)
 Object.assign(window, { pageStateCache });

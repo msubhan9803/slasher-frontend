b1bf43723902c6dbf6b635b82d6e9979f456cba0
Sahil Rajput <sahilrajput03@gmail.com>
2023-06-03 18:12:19 +0530

    SD-1263: Fix ads not loading issue on android apk build.

--- capacitor-configs-template/static-build.capacitor.config.ts
+++ capacitor-configs-template/static-build.capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'tv.slasher.app',
   appName: 'Slasher',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    hostname: 'cap.android.slasher.tv',
+    cleartext: true,
+  }
 };
 
 export default config;

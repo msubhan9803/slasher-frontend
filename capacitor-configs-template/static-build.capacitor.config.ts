52c0b76791bb22183ca8367b5125d09c4a7266f3
Avadh Ladani <avadh.ladani353@gmail.com>
2023-06-05 18:53:06 +0530

    change android bundle name

--- capacitor-configs-template/static-build.capacitor.config.ts
+++ capacitor-configs-template/static-build.capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'tv.slasher.app',
+  appId: 'com.sdei.slasher',
   appName: 'Slasher',
   webDir: 'build',
   bundledWebRuntime: false
--- src/components/pushNotification.tsx
+++ src/components/pushNotification.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import {
   ActionPerformed, PushNotifications, Token,
 } from '@capacitor/push-notifications';
+import { Capacitor } from '@capacitor/core';
 import Cookies from 'js-cookie';
 import { Device } from '@capacitor/device';
 import { updateUserDeviceToken } from '../api/users';
@@ -10,38 +11,40 @@ import { urlForNotification } from '../utils/notification-url-utils';
 
 function PushNotificationWrapper() {
   useEffect(() => {
-    PushNotifications.requestPermissions().then((result) => {
-      if (result.receive === 'granted') {
-        PushNotifications.register();
-      }
-    });
+    if (Capacitor.isNativePlatform()) {
+      PushNotifications.requestPermissions().then((result) => {
+        if (result.receive === 'granted') {
+          PushNotifications.register();
+        }
+      });
 
-    PushNotifications.addListener('registration', async (token: Token) => {
-      // Send the token to your server for further processing, if needed
-      const deviceToken = token.value;
-      if (deviceToken !== Cookies.get('deviceToken')) {
-        const deviceId = await Device.getId();
-        updateUserDeviceToken(deviceId.uuid, deviceToken);
-      }
-      Cookies.set('deviceToken', deviceToken);
-    });
+      PushNotifications.addListener('registration', async (token: Token) => {
+        // Send the token to your server for further processing, if needed
+        const deviceToken = token.value;
+        if (deviceToken !== Cookies.get('deviceToken')) {
+          const deviceId = await Device.getId();
+          updateUserDeviceToken(deviceId.uuid, deviceToken);
+        }
+        Cookies.set('deviceToken', deviceToken);
+      });
 
-    // PushNotifications.addListener('pushNotificationReceived',
-    // (notification: PushNotificationSchema) => {
-    //   // Handle the received push notification
-    //   const { data } = notification.data;
-    //   const notificationData = JSON.parse(data);
-    // });
+      // PushNotifications.addListener('pushNotificationReceived',
+      // (notification: PushNotificationSchema) => {
+      //   // Handle the received push notification
+      //   const { data } = notification.data;
+      //   const notificationData = JSON.parse(data);
+      // });
 
-    PushNotifications.addListener(
-      'pushNotificationActionPerformed',
-      async (notification: ActionPerformed) => {
-        const { data } = notification.notification.data;
-        const notificationData = JSON.parse(data);
-        window.location.href = urlForNotification(notificationData);
-        // navigate(urlForNotification(notificationData));
-      },
-    );
+      PushNotifications.addListener(
+        'pushNotificationActionPerformed',
+        async (notification: ActionPerformed) => {
+          const { data } = notification.notification.data;
+          const notificationData = JSON.parse(data);
+          window.location.href = urlForNotification(notificationData);
+          // navigate(urlForNotification(notificationData));
+        },
+      );
+    }
   }, []);
 
   return (

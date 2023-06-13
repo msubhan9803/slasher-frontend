aaf9092ff89e74b78c3430d72cf9d08fa237b493
Eric O <eric@ejdigitalmedia.com>
2023-06-13 17:09:51 -0400

    Add Firebase startup warning when no valid Firebase config is found

--- src/app/providers/initFirebase.ts
+++ src/app/providers/initFirebase.ts
@@ -1,6 +1,9 @@
+import { Logger } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 
 type FirebaseAdmin = typeof admin;
+
+const logger = new Logger('initFirebase');
 
 // Initialize Firebase app
 export const firebaseInstance = {
@@ -9,10 +12,12 @@ export const firebaseInstance = {
 
 export function initiateFirebase(firebaseServerKey: string) {
   const serviceAccount = firebaseServerKey && JSON.parse(firebaseServerKey);
-  if (serviceAccount) {
+  if (serviceAccount && serviceAccount.project_id) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
+  } else {
+    logger.warn('Firebase instance was not initialized because a valid config was not found.');
   }
   firebaseInstance.admin = admin;
 }

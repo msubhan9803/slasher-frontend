ae85a821844ba94ee443a43a5ba4a565f3ae2d2d
Avadh Ladani <avadh.ladani353@gmail.com>
2023-06-29 19:40:40 +0530

    hide back button in create post

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -8,7 +8,7 @@ export const urlForMovie = (id:string) => `/app/movies/${id}/details`;
 export const urlForEvent = (id:string) => `/app/events/${id}`;
 
 export const isHomePage = (pathname: string) => pathname === '/app/home'; // e.g, pathname = '/app/home'
-export const isPostDetailsPage = (pathname: string) => new RegExp('/.+/posts/.+').test(pathname); // e.g, pathname = '/slasher-test-user1/posts/6450086566a60138c4e2b293'
+export const isPostDetailsPage = (pathname: string) => new RegExp('/.+/posts/[a-f\\d]{24}/.+').test(pathname); // e.g, pathname = '/slasher-test-user1/posts/6450086566a60138c4e2b293'
 export const isNewsPartnerPage = (pathname: string) => new RegExp('/app/news/partner/.*').test(pathname); // e.g, pathname = '/app/news/partner/6036639a657a566248b973f7'
 export const isMovieDetailsPageSubRoutes = (pathname: string) => new RegExp('/app/movies/[a-f\\d]{24}/.+').test(pathname);
 export const isEventDetailsPage = (pathname: string) => new RegExp('/app/events/[a-f\\d]{24}$').test(pathname); // e.g., pathname = 'http://localhost:3000/app/events/5

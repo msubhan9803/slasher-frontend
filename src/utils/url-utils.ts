dc0a93a568c7d1df3dd1cab035b032098974d70c
Sahil Rajput <sahilrajput03@gmail.com>
2023-07-02 20:59:24 +0530

    Refactor url-utils.ts file.

--- src/utils/url-utils.ts
+++ src/utils/url-utils.ts
@@ -7,12 +7,14 @@ export const urlForMovie = (id:string) => `/app/movies/${id}/details`;
 
 export const urlForEvent = (id:string) => `/app/events/${id}`;
 
+export const MONGODB_ID = '[a-f\\d]{24}';
+
 export const isHomePage = (pathname: string) => pathname === '/app/home'; // e.g, pathname = '/app/home'
-export const isPostDetailsPage = (pathname: string) => new RegExp('/.+/posts/[a-f\\d]{24}').test(pathname); // e.g, pathname = '/slasher-test-user1/posts/6450086566a60138c4e2b293'
-export const isNewsPartnerPage = (pathname: string) => new RegExp('/app/news/partner/.*').test(pathname); // e.g, pathname = '/app/news/partner/6036639a657a566248b973f7'
-export const isMovieDetailsPageSubRoutes = (pathname: string) => new RegExp('/app/movies/[a-f\\d]{24}/.+').test(pathname);
-export const isEventDetailsPage = (pathname: string) => new RegExp('/app/events/[a-f\\d]{24}$').test(pathname); // e.g., pathname = 'http://localhost:3000/app/events/5
-export const isConversationPage = (pathname: string) => new RegExp('/app/messages/conversation/[a-f\\d]{24}$').test(pathname); // e.g., pathname = 'http://localhost:3000/app/events/5
+export const isPostDetailsPage = (pathname: string) => new RegExp(`/.+/posts/${MONGODB_ID}$`).test(pathname); // e.g, pathname = '/slasher-test-user1/posts/6450086566a60138c4e2b293'
+export const isNewsPartnerPage = (pathname: string) => new RegExp(`/app/news/partner/${MONGODB_ID}`).test(pathname); // e.g, pathname = '/app/news/partner/6036639a657a566248b973f7'
+export const isMovieDetailsPageSubRoutes = (pathname: string) => new RegExp(`/app/movies/${MONGODB_ID}/.+`).test(pathname);
+export const isEventDetailsPage = (pathname: string) => new RegExp(`/app/events/${MONGODB_ID}$`).test(pathname); // e.g., pathname = 'http://localhost:3000/app/events/5
+export const isConversationPage = (pathname: string) => new RegExp(`/app/messages/conversation/${MONGODB_ID}$`).test(pathname); // e.g., pathname = 'http://localhost:3000/app/events/5
 export const isUserProfilePage = (pathname: string) => new RegExp('^(?!/app).*').test(pathname); // e.g, pathname = '/slasher-test-user1/posts/6450086566a60138c4e2b293'
 
 export const showBackButtonInIos = (pathname: string) => [isPostDetailsPage, isNewsPartnerPage,

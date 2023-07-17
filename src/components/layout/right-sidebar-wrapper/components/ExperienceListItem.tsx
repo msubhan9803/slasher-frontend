d19704b872086f8a5cbf9a28430247a55057dbe7
Avadh Ladani <avadh.ladani353@gmail.com>
2023-07-17 18:42:48 +0530

    add ... after 2 lines of movie name

--- src/components/layout/right-sidebar-wrapper/components/ExperienceListItem.tsx
+++ src/components/layout/right-sidebar-wrapper/components/ExperienceListItem.tsx
@@ -29,6 +29,11 @@ const YearAndThumbRating = styled.div`
 const MovieTitle = styled.div`
   font-size: 0.75rem;
   font-weight: 500;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2; 
+  -webkit-box-orient: vertical;
 `;
 
 interface Props {

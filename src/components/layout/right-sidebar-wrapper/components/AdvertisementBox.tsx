6c231173955939ca79a4289563cc670770d4756e
Sahil Rajput <sahilrajput03@gmail.com>
2023-07-21 03:12:28 +0530

    SD-1462: Disable right-sidebar global ad for capacitorapp

--- src/components/layout/right-sidebar-wrapper/components/AdvertisementBox.tsx
+++ src/components/layout/right-sidebar-wrapper/components/AdvertisementBox.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PubWiseAd from '../../../ui/PubWiseAd';
 import { GLOBAL_RIGHT_NAV_DIV_ID } from '../../../../utils/pubwise-ad-units';
+import { isNativePlatform } from '../../../../constants';
 
 function AdvertisementBox() {
   return (
     <div>
-      <PubWiseAd id={GLOBAL_RIGHT_NAV_DIV_ID} />
+      { !isNativePlatform && <PubWiseAd id={GLOBAL_RIGHT_NAV_DIV_ID} /> }
     </div>
   );
 }

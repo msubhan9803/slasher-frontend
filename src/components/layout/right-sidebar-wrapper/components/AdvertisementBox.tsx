7b0725000d3673d98679b1fe0d8aadd05807ee16
Sahil Rajput <sahilrajput03@gmail.com>
2023-07-21 03:40:56 +0530

    SD-1462: Disable rightsidebar global ad for mobile-web as well.

--- src/components/layout/right-sidebar-wrapper/components/AdvertisementBox.tsx
+++ src/components/layout/right-sidebar-wrapper/components/AdvertisementBox.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import PubWiseAd from '../../../ui/PubWiseAd';
+import { LG_MEDIA_BREAKPOINT } from '../../../../constants';
 import { GLOBAL_RIGHT_NAV_DIV_ID } from '../../../../utils/pubwise-ad-units';
-import { isNativePlatform } from '../../../../constants';
 
 function AdvertisementBox() {
+  const isRightSideBarVisible = useMediaQuery({ query: `(min-width: ${LG_MEDIA_BREAKPOINT})` });
+
   return (
     <div>
-      { !isNativePlatform && <PubWiseAd id={GLOBAL_RIGHT_NAV_DIV_ID} /> }
+      { isRightSideBarVisible && <PubWiseAd id={GLOBAL_RIGHT_NAV_DIV_ID} /> }
     </div>
   );
 }

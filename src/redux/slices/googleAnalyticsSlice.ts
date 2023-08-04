10ff736bdff62056f825edd022a75c2af97cb934
Sahil Rajput <sahilrajput03@gmail.com>
2023-08-05 03:46:11 +0530

    SD-1491: Fix eslint issue.

--- src/redux/slices/googleAnalyticsSlice.ts
+++ src/redux/slices/googleAnalyticsSlice.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 export const googleAnalytics = createSlice({
   name: 'googleAnalytics',

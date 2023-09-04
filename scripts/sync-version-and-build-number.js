030428edbeaa29ad5db1771365c50a1b4ff4c66e
Eric O <eric@ejdigitalmedia.com>
2023-09-04 13:32:13 -0400

    Make sure that npm_package_version is set properly in CI build environments

--- scripts/sync-version-and-build-number.js
+++ scripts/sync-version-and-build-number.js
@@ -1,4 +1,6 @@
-const { version, capacitorAppBuildNumber } = require(__dirname + '/../package.json');
+/* eslint-disable no-console */
+/* eslint-disable import/no-dynamic-require */
+const { version, capacitorAppBuildNumber } = require(`${__dirname}/../package.json`);
 const fs = require('fs');
 
 console.log('Syncing version and build number to android and iOS projects...');
@@ -6,14 +8,14 @@ console.log(`Version: ${version}`);
 console.log(`Build number: ${capacitorAppBuildNumber}`);
 
 // Update Android app version
-const androidBuildGradleFilePath = __dirname + '/../android/app/build.gradle';
+const androidBuildGradleFilePath = `${__dirname}/../android/app/build.gradle`;
 let androidBuildGradleContent = fs.readFileSync(androidBuildGradleFilePath).toString();
 androidBuildGradleContent = androidBuildGradleContent.replace(/versionCode \d+/, `versionCode ${capacitorAppBuildNumber}`);
 androidBuildGradleContent = androidBuildGradleContent.replace(/versionName "[^"]+"/, `versionName "${version}"`);
 fs.writeFileSync(androidBuildGradleFilePath, androidBuildGradleContent);
 
 // Update iOS app version
-const iosAppXcodeProjectFilePath = __dirname + '/../ios/App/App.xcodeproj/project.pbxproj';
+const iosAppXcodeProjectFilePath = `${__dirname}/../ios/App/App.xcodeproj/project.pbxproj`;
 let iosAppXcodeProjContent = fs.readFileSync(iosAppXcodeProjectFilePath).toString();
 iosAppXcodeProjContent = iosAppXcodeProjContent.replace(/CURRENT_PROJECT_VERSION = \d+;/g, `CURRENT_PROJECT_VERSION = ${capacitorAppBuildNumber};`);
 iosAppXcodeProjContent = iosAppXcodeProjContent.replace(/MARKETING_VERSION = [^;]+;/g, `MARKETING_VERSION = "${version}";`);

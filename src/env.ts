f805ca4806fb8c9566183741d3df2aab315032c1
Sahil Rajput <sahilrajput03@gmail.com>
2023-10-05 04:39:11 +0530

    SD-00: Refactor code for code readability.

--- src/env.ts
+++ src/env.ts
@@ -4,3 +4,4 @@ export const analyticsId = process.env.REACT_APP_GOOGLE_ANALYTICS_PROPERTY_ID;
 export const enableDevFeatures = process.env.REACT_APP_ENABLE_DEV_ELEMENTS === 'true';
 export const enableADs = process.env.REACT_APP_ENABLE_ADS === 'true';
 export const isDevelopmentServer = process.env.NODE_ENV === 'development';
+export const isProductionServer = process.env.NODE_ENV === 'production';

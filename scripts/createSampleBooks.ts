3ad311f884a546f5473575e592ff675a3b337f0f
Sahil Rajput <sahilrajput03@gmail.com>
2023-10-24 20:21:05 +0530

    SD-1431: Update code for books sync.

--- scripts/createSampleBooks.ts
+++ scripts/createSampleBooks.ts
@@ -5,6 +5,7 @@ import mongoose, { ConnectionStates, Model } from 'mongoose';
 import { createApp } from './createApp';
 import { BooksService } from '../src/books/providers/books.service';
 import { Book, BookDocument } from '../src/schemas/book/book.schema';
+import { BookUserStatus } from '../src/schemas/bookUserStatus/bookUserStatus.schema';
 
 // Run this file via below command:
 // NODE_ENV=development npx ts-node ./scripts/createSampleBooks.ts
@@ -15,12 +16,20 @@ const deleteAllBooks = async (app: INestApplication) => {
   await booksModel.deleteMany({});
   console.log('Deleted all books in DB');
 };
+const deleteAllBooksUserStatus = async (app: INestApplication) => {
+  const bookUserStatus = app.get<Model<BookUserStatus>>(getModelToken(BookUserStatus.name));
+  // Delete all documents in `books` collection
+  await bookUserStatus.deleteMany({});
+  console.log('Deleted all bookUserStatus in DB');
+};
 
 const createSampleBooks = async (app: INestApplication) => {
   console.log('STARTED: createSampleBooks');
   const booksService = app.get<BooksService>(BooksService);
-  // ! Please use this only when needed along with caution ~ Sahil
+  // ! Please use this only when needed along with caution
   await deleteAllBooks(app);
+  // ! Please use this only when needed along with caution
+  await deleteAllBooksUserStatus(app);
 
   const { success, error } = await booksService.syncWithOpenLibrary();
   if (error) {

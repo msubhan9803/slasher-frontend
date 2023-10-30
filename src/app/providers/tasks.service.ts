fa6c2325a2492469dc5943349501299b43cd5db6
ladaniavadh <avadh.ladani353@gmail.com>
2023-10-30 11:53:10 +0530

    update the time for books cron

--- src/app/providers/tasks.service.ts
+++ src/app/providers/tasks.service.ts
@@ -38,7 +38,7 @@ export class TasksService {
     }
   }
 
-  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, {
+  @Cron(CronExpression.EVERY_DAY_AT_4AM, {
     name: 'syncWithTheBookDb',
     timeZone: 'America/New_York',
   })
